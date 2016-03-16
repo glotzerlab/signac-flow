3c0a12dc47a016cac9c9c3bc092aacca26debd62
Carl Simon Adorf <csadorf@umich.edu>
2016-03-16 13:41:16 -0400

    Add a setup.py install script.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import sys
+
+if sys.version_info < (2,7,0):
+    print("Error: signac requires python version >= 2.7.x.")
+    sys.exit(1)
+
+from setuptools import setup, find_packages
+
+setup(
+    name='signac-flow',
+    version='0.0.0',
+    packages=find_packages(),
+    zip_safe=True,
+
+    author='Carl Simon Adorf',
+    author_email='csadorf@umich.edu',
+    description="Workflow management based on signac.",
+    keywords='workflow management singac framework database',
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+)

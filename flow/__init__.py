cdd61e6a44c4936e16fdce14f2daf05ff76f237a
Carl Simon Adorf <csadorf@umich.edu>
2016-03-17 08:41:27 -0400

    Bump to version 0.1.1.

--- flow/__init__.py
+++ flow/__init__.py
@@ -8,7 +8,7 @@ from . import scheduler
 from . import manage
 from . project import FlowProject
 
-__version__ = '0.0.1'
+__version__ = '0.1.1'
 
 __all__ = [
     'environment',
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='signac-flow',
-    version='0.0.1',
+    version='0.1.1',
     packages=find_packages(),
     zip_safe=True,
 

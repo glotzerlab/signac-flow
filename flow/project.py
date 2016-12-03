c888feae95c34fe705334322b04eaf9d788a6b14
Carl Simon Adorf <csadorf@umich.edu>
2016-12-03 17:44:11 -0500

    Make print_status() fully honor the file argument.
    Some of the status output was errorneously printed to screen
    instead to the file.

--- flow/project.py
+++ flow/project.py
@@ -544,7 +544,6 @@ class FlowProject(signac.contrib.Project):
 
     def print_detailed(self, stati, parameters=None,
                        skip_active=False, param_max_width=-1,
-                       use_colors=True,
                        file=sys.stdout):
         "Print the project's detailed status."
         table_header = [self._tr(self._alias(s)) for s in ('job_id', 'status', 'next_operation', 'labels')]
@@ -555,10 +554,10 @@ class FlowProject(signac.contrib.Project):
                 for status in stati if not (skip_active and status['active']))
         print(util.tabulate.tabulate(rows, headers=table_header), file=file)
         if abbreviate.table:
-            print()
+            print(file=file)
             print(self._tr("Abbreviations used:"), file=file)
             for a in sorted(abbreviate.table):
-                print('{}: {}'.format(a, abbreviate.table[a]))
+                print('{}: {}'.format(a, abbreviate.table[a]), file=file)
 
     def export_job_stati(self, collection, stati):
         for status in stati:
@@ -625,7 +624,7 @@ class FlowProject(signac.contrib.Project):
         title = "{} '{}':".format(self._tr("Status project"), self)
         print('\n' + title, file=file)
         if overview:
-            self.print_overview(stati, max_lines=overview_max_lines)
+            self.print_overview(stati, max_lines=overview_max_lines, file=file)
         if detailed:
             print(file=file)
             print(self._tr("Detailed view:"), file=file)

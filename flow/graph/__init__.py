45e1fa50c09dbb0a5a252c66b3525505d7b926ef
Paul Dodd <pdodd@umich.edu>
2017-01-11 16:11:09 -0500

    initial api defintions

--- flow/graph/__init__.py
+++ flow/graph/__init__.py
@@ -3,4 +3,4 @@
 # This software is licensed under the BSD 3-Clause License.
 from . import graph
 
-__all__ = ['graph'];
+__all__ = ['graph']
--- flow/graph/graph.py
+++ flow/graph/graph.py
@@ -2,5 +2,21 @@
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 
+import networkx as nx
+
 class FlowGraph:
-    pass;
+    def __init__(self):
+        self._operations = None;
+        self._graph = nx.Graph();
+
+    def add_operation(self, callback, prereqs, postcond):
+        pass;
+
+    def next_operations(self, job):
+        pass;
+
+    def _build_graph(self):
+        pass;
+
+    def get_operation_chain(self, job, condition):
+        pass;

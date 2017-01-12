95f2d1ca67f0ba97f063c99fe26ece9d746830e0
Carl Simon Adorf <csadorf@umich.edu>
2017-01-12 08:51:51 -0500

    Convert graph package to graph module.

--- flow/graph/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2016 The Regents of the University of Michigan
-# All rights reserved.
-# This software is licensed under the BSD 3-Clause License.
-from . import graph
-from . graph import FlowGraph
-
-__all__ = ['graph']

ea84b60d877124dc141a545a8011e8a6765ff659
Carl Simon Adorf <csadorf@umich.edu>
2017-02-16 08:08:38 -0600

    Remove APScheduler driver.
    The apscheduler package has proven to be too unreliable to justify
    further support.

--- flow/scheduler.py
+++ flow/scheduler.py
@@ -2,24 +2,10 @@
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 import warnings
+
+from .fakescheduler import FakeScheduler
 from .moab import MoabScheduler
 from .slurm import SlurmScheduler
-from .fakescheduler import FakeScheduler
 
 
-try:
-    from .apscheduler import APScheduler  # noqa
-except ImportError:
-    warnings.warn("Failed to import apscheduler. "
-                  "The test scheduler will not be available.",
-                  ImportWarning)
-
-    class APScheduler(object):
-        """This is a mock class.
-
-        Install apscheduler to enable the test environment."""
-
-        def __init__(self, *args, **kwargs):
-            raise ImportError("Install apscheduler to enable this scheduler.")
-
-__all__ = ['MoabScheduler', 'SlurmScheduler', 'APScheduler', 'FakeScheduler']
+__all__ = ['FakeScheduler', 'MoabScheduler', 'SlurmScheduler']

3911f66132441e08a07919a10bc3f8900cbd3557
Carl Simon Adorf <csadorf@umich.edu>
2017-05-18 18:43:05 -0400

    Fix util __init__ module.

--- flow/util/__init__.py
+++ flow/util/__init__.py
@@ -3,7 +3,4 @@
 # This software is licensed under the BSD 3-Clause License.
 from . import tabulate, tqdm
 
-from .operations import run
-from .operations import redirect_log
-
-__all__ = ['tabulate', 'tqdm', 'run', 'redirect_log']
+__all__ = ['tabulate', 'tqdm']

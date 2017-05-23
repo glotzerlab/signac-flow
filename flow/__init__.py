7539b2cb0e442ce2754231283e1c405c3b3b4eb5
Carl Simon Adorf <csadorf@umich.edu>
2017-05-22 22:57:09 -0400

    Bump up to version 0.5.0b2.

--- flow/__init__.py
+++ flow/__init__.py
@@ -22,7 +22,7 @@ from .environment import get_environment
 from .operations import run
 from .operations import redirect_log
 
-__version__ = '0.5.0b1'
+__version__ = '0.5.0b2'
 
 __all__ = [
     'environment',

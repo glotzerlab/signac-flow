277bea3e6147c0ad60d4cc2c24ff03d0a09dfb11
Carl Simon Adorf <csadorf@umich.edu>
2017-08-01 12:17:29 -0400

    Exit with error for using timeout in serial run with python2.7.
    Using timeouts for forked processes is not as well supported for python
    version 2.7.x.

--- flow/project.py
+++ flow/project.py
@@ -573,6 +573,9 @@ class FlowProject(with_metaclass(_FlowProjectClass, signac.contrib.Project)):
         _run = print if pretend else _execute
 
         if np == 1:      # serial execution
+            if six.PY2 and timeout is not None:
+                raise RuntimeError(
+                    "Using a timeout with serial execution is not supported for Python version 2.7.")
             for cmd in tqdm(cmds) if progress else cmds:
                 _run(cmd, timeout=timeout)
         elif six.PY2:   # parallel execution (py27)

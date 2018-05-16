5ed670800d66a04f42bad8ecd1ea3e2ba9c195c2
Vyas Ramasubramani <vramasub@umich.edu>
2018-05-16 10:44:11 -0500

    inherit from slurm in stampede

--- flow/templates/stampede2.sh
+++ flow/templates/stampede2.sh
@@ -1,27 +1,23 @@
-{% extends "base_script.sh" %}
-{% block header %}
-#!/bin/bash
-#SBATCH --job-name="{{ id }}"
-{% set account = 'account'|get_config_value(ns=environment) %}
-{% if account is not none %}
-#SBATCH -A {{ account }}
-{% endif %}
-{% if job_output is not none %}
-#SBATCH --output={{ job_output }}
-#SBATCH --error={{ job_output }}
-{% endif %}
-{% if walltime %}
-#SBATCH -t {{ walltime|format_timedelta }}
-{% endif %}
+{% extends "slurm.sh" %}
+{# Must override this block before header block is created #}
+{% block tasks %}
 {% if operations|map(attribute='directives.tpn')|identical %}
 {# Easy way to get the value #}
 {% set tpn = operations|map(attribute='directives.tpn')|max %}
 {% else %}
 {% raise "Cannot submit operations requiring different tpn." %}
 {% endif %}
-#SBATCH --partition={{ partition }}
-{% cpn = 48 if 'skx' in partition else 68 %}
+{% set cpn = 48 if 'skx' in partition else 68 %}
 {% set nn = (num_tasks/cpn)|round(method='ceil')|int %}
 #SBATCH --nodes={{ nn }}
 #SBATCH --ntasks-per-node={{ tpn }}
+{% endblock %}
+
+{% block header %}
+{{ super () -}}
+{% set account = 'account'|get_config_value(ns=environment) %}
+{% if account is not none %}
+#SBATCH -A {{ account }}
+{% endif %}
+#SBATCH --partition={{ partition }}
 {% endblock %}

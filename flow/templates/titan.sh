f481ad230e393cd57ddcc985dfc95675e20936fc
Vyas Ramasubramani <vramasub@umich.edu>
2018-05-16 10:54:57 -0400

    Eos and Titan use Torque...

--- flow/templates/titan.sh
+++ flow/templates/titan.sh
@@ -1,8 +1,8 @@
-{% extends "slurm.sh" %}
+{% extends "torque.sh" %}
 {% block header %}
 {{ super() }}
 {% set account = 'account'|get_config_value(ns=environment) %}
 {% if account is not none %}
-#SBATCH -A {{ account }}
+#PBS -A {{ account }}
 {% endif %}
 {% endblock %}

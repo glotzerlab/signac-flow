6bbc28b85c45cb77e8d5985ae51cb3de33388b84
Carl Simon Adorf <csadorf@umich.edu>
2018-05-17 13:16:59 -0700

    Update comet template.

--- flow/templates/comet.sh
+++ flow/templates/comet.sh
@@ -1,7 +1,7 @@
 {% extends "slurm.sh" %}
 {# Must override this block before header block is created #}
 {% block tasks %}
-{% if partition == 'shared' %}
+{% if 'shared' in partition %}
 {% if num_tasks > 24 %}
 {% raise "You cannot use more than 24 cores on the 'shared' partitions." %}
 {% else %}
@@ -11,11 +11,17 @@
 {% else %}
 {% set nn = (num_tasks/24)|round(method='ceil')|int %}
 {% set ntasks = 24 if num_tasks > 24 else num_tasks %}
+{% set node_util = num_tasks / (24 * nn) %}
+{% if not force and node_util < 0.9 %}
+{% raise "Bad node utilization!!" %}
+{% endif %}
 #SBATCH --nodes={{ nn }}
-#SBATCH --ntasks={{ ntasks }}
+#SBATCH --ntasks-per-node={{ ntasks }}
+{% endif %}
+{% if partition == 'gpu-shared' %}
+#SBATCH --gres=gpu:p100:{{ num_tasks }}
 {% endif %}
 {% endblock %}
-
 {% block header %}
 {{ super () -}}
 {% set account = 'account'|get_config_value(ns=environment) %}

6c9bde47954bf47f3b5f74c9108e60dc45b9d223
Vyas <vramasub@umich.edu>
2018-11-01 12:56:14 -0400

    Import print function for 2.7

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2018 The Regents of the University of Michigan
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
+from __future__ import print_function
+
 import unittest
 
 import signac

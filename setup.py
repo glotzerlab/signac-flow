f88c8fe448c4c52dfcfb9be742f114126201c776
Carl Simon Adorf <csadorf@umich.edu>
2019-03-14 13:27:58 -0400

    Bump version: 0.6.4 → 0.7.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='signac-flow',
-    version='0.6.4',
+    version='0.7.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,

0a9d0de6f3fe6d686c1d89ad4d3048c49d729afd
Carl Simon Adorf <csadorf@umich.edu>
2019-03-14 10:09:47 -0400

    Harmonize requirements specifications.

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ requirements = [
     # The core package.
     'signac>=0.9.0',
     # For the templated generation of (submission) scripts.
-    'jinja2',
+    'jinja2>=2.8',
     # To enable the parallelized execution of operations across processes.
     'cloudpickle',
     # To define IntEnum in flow/scheduling/base.py.
-    'enum34;python_version<"3.4"',
+    'enum34;python_version<3.4',
     # Deprecation management
     'deprecation>=2',
 ]

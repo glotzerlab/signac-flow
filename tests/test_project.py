7401d91baed05e48125fd9a68726ae69402ae0eb
Carl Simon Adorf <csadorf@umich.edu>
2019-03-25 16:16:52 -0400

    Fix Python 2.7 incompatibility with python path manipulation.
    Squashed commit of the following:
    
    commit c9d3546a08399c27da6973f75549ccdda3dd7e16
    Author: Carl Simon Adorf <csadorf@umich.edu>
    Date:   Mon Mar 25 16:10:45 2019 -0400
    
        Add signac path to PYTHONPATH.
    
    commit df2b6ceb986b07acd1ec0322fec8cbcc4ad847a8
    Author: Carl Simon Adorf <csadorf@umich.edu>
    Date:   Mon Mar 25 16:00:44 2019 -0400
    
        Revert "Print sys.path in define_test_project module."
    
        This reverts commit 935bb9a547486181446d42ec9fc5f04355696251.
    
    commit 591dd27214737ddca3c91b87af6269f742aceb53
    Author: Carl Simon Adorf <csadorf@umich.edu>
    Date:   Mon Mar 25 15:58:02 2019 -0400
    
        Use absolute path for PYTHONPATH manipulation.
    
    commit 935bb9a547486181446d42ec9fc5f04355696251
    Author: Carl Simon Adorf <csadorf@umich.edu>
    Date:   Mon Mar 25 15:52:45 2019 -0400
    
        Print sys.path in define_test_project module.
    
    commit e1248de14f53245637312bdcc814cb0af7ad8478
    Author: Carl Simon Adorf <csadorf@umich.edu>
    Date:   Mon Mar 25 15:51:58 2019 -0400
    
        Do not use 'shell=True'.
    
    commit cdacd6e32f2069f92f3231331af467c98628a3aa
    Author: Carl Simon Adorf <csadorf@umich.edu>
    Date:   Mon Mar 25 15:46:03 2019 -0400
    
        Add 'flow' parent directory to path.
    
    commit 513f3a9efe798d53ba7e54b4be2f9403c6f49b58
    Author: Carl Simon Adorf <csadorf@umich.edu>
    Date:   Mon Mar 25 15:34:12 2019 -0400
    
        Use shell execution for MockScheduler execution step.

--- tests/test_project.py
+++ tests/test_project.py
@@ -102,7 +102,9 @@ class MockScheduler(Scheduler):
                 break
         cid = uuid.uuid4()
         cls._jobs[cid] = ClusterJob(_id, status=JobStatus.submitted)
-        pythonpath = ':'.join(os.environ.get('PYTHONPATH', '').split(':') + flow.__path__)
+        signac_path = os.path.dirname(os.path.dirname(os.path.abspath(signac.__file__)))
+        flow_path = os.path.dirname(os.path.dirname(os.path.abspath(flow.__file__)))
+        pythonpath = ':'.join(os.environ.get('PYTHONPATH', []) + [signac_path, flow_path])
         cls._scripts[cid] = 'export PYTHONPATH={}\n'.format(pythonpath) + script
         return JobStatus.submitted
 

28d5e80edad8b25e998b15908c576772e91eba45
Pengji Zhou <zhoupj@umich.edu>
2019-05-08 22:32:17 -0400

    bug fix, move overview section outside

--- flow/templates/print_status.sh
+++ flow/templates/print_status.sh
@@ -1,8 +1,8 @@
 {# The following variables are available to all scripts. #}
+{% set format_progress_bar = ("%%-%ss  %%-%ss" | format(label_length,bar_length,)) %}
 {% if detailed %}
 {% set alias = scheduler_status_code %}
 {% set operation_length = [operation_length,5] | max %}
-{% set format_progress_bar = ("%%-%ss  %%-%ss" | format(label_length,bar_length,)) %}
 {% set format_job_id = ("%%-%ss  " | format(id_length,)) %}
 {% set format_operation = ("%%-%ss %%3s  " | format(operation_length,)) %}
 {% set format_operation_title = ("%%-%ss  " | format(operation_length+4,)) %}
@@ -30,7 +30,6 @@ Total # of jobs: {{'%s' |format(jobs_count)}}
 {{ format_progress_bar | format(label[0], label[1] | draw_progressbar(jobs_count))}}
 {% endfor %}
 {% endif %}
-
 {% if detailed %}
 # Detailed View:
 {{ format_head | format('job_id', 'operation', para_head, 'labels',) }}
@@ -52,6 +51,6 @@ Total # of jobs: {{'%s' |format(jobs_count)}}
 {% endif %}
 {% endfor %}
 {% endfor %}
+[U]:unknown [R]:registered [Q]:queued [A]:active [I]:inactive [!]:requires_attention
 {% endif %}
 {% endblock %}
-[U]:unknown [R]:registered [Q]:queued [A]:active [I]:inactive [!]:requires_attention

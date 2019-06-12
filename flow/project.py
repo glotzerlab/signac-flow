glotzerlab/signac-flow

commit 86a27e997a289ab27ce57355711e0f50b1974124

Feature/select operations with regex (#89)
* Refactor run_operations() to use the _get_pending_operations() function.

To unify operations-selection across script/run/submit.

* Enable selection of operations with a regex search pattern.

This patch enables the selection of operations for run/script/submit
with a regular expression.

* Add unit test for operations-selection for FlowProject.run().

* Catch a bad names argument (string instead of sequence of strings).

Related to issue #58.

flow/project.py
@@ -62,6 +62,7 @@ from .util import template_filters as tf
 from .util.misc import add_cwd_to_environment_pythonpath
 from .util.misc import switch_to_directory
 from .util.misc import TrackGetItemDict
+from .util.misc import fullmatch
 from .util.progressbar import with_progressbar
 from .util.translate import abbreviate
 from .util.translate import shorten
@@ -1371,7 +1372,7 @@ class FlowProject(six.with_metaclass(_FlowProjectClass,
         if timeout is not None and timeout < 0:
             timeout = None
         if operations is None:
-            operations = [op for job in self for op in self._job_operations([job], False)]
+            operations = list(self._get_pending_operations(self))
         else:
             operations = list(operations)   # ensure list
 
@@ -1520,6 +1521,12 @@ class FlowProject(six.with_metaclass(_FlowProjectClass,
         if num and num < 0:
             num = None
 
+        # The 'names' argument must be a sequence, not a string.
+        if isinstance(names, six.string_types):
+            raise ValueError(
+                "The names argument of FlowProject.run() must be a sequence of strings, "
+                "not a string.")
+
         messages = list()
 
         def log(msg, lvl=logging.INFO):
@@ -1593,14 +1600,10 @@ class FlowProject(six.with_metaclass(_FlowProjectClass,
 
     def _get_pending_operations(self, jobs, operation_names=None):
         "Get all pending operations for the given selection."
-        operation_names = None if operation_names is None else set(operation_names)
-
-        for op in self.next_operations(*jobs):
-            if operation_names and op.name not in operation_names:
-                continue
-            if not self.eligible_for_submission(op):
-                continue
-            yield op
+        assert not isinstance(operation_names, six.string_types)
+        for op in self.next_operations(* jobs):
+            if operation_names is None or any(fullmatch(n, op.name) for n in operation_names):
+                yield op
 
     @contextlib.contextmanager
     def _potentially_buffered(self):
@@ -1821,7 +1824,8 @@ class FlowProject(six.with_metaclass(_FlowProjectClass,
 
         # Gather all pending operations.
         with self._potentially_buffered():
-            operations = self._get_pending_operations(jobs, names)
+            operations = (op for op in self._get_pending_operations(jobs, names)
+                          if self.eligible_for_submission(op))
             if num is not None:
                 operations = list(islice(operations, num))
 
@@ -1928,7 +1932,6 @@ class FlowProject(six.with_metaclass(_FlowProjectClass,
             '-o', '--operation',
             dest='operation_name',
             nargs='+',
-            choices=operations,
             help="Only select operations that match the given operation name(s).")
         selection_group.add_argument(
             '-n', '--num',

flow/util/misc.py
@@ -2,6 +2,7 @@
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 import os
+import re
 import errno
 import json
 import argparse
@@ -192,3 +193,9 @@ class TrackGetItemDict(dict):
     def keys_used(self):
         "Return all keys that have been accessed."
         return self._keys_used.copy()
+
+
+# Remove this after we drop Python 2.7 support:
+def fullmatch(regex, string, flags=0):
+    """Emulate python-3.4 re.fullmatch()."""
+    return re.match("(?:" + regex + r")\Z", string, flags=flags)

tests/test_project.py
@@ -124,7 +124,13 @@ class MockScheduler(Scheduler):
                         with tempfile.NamedTemporaryFile() as tmpfile:
                             tmpfile.write(cls._scripts[cid].encode('utf-8'))
                             tmpfile.flush()
-                            subprocess.check_call(['/bin/bash', tmpfile.name])
+                            if six.PY2:
+                                with open(os.devnull, 'w') as devnull:
+                                    subprocess.check_call(
+                                        ['/bin/bash', tmpfile.name], stderr=devnull)
+                            else:
+                                subprocess.check_call(
+                                    ['/bin/bash', tmpfile.name], stderr=subprocess.DEVNULL)
                     except Exception:
                         job._status = JobStatus.error
                         raise
@@ -539,6 +545,25 @@ class ExecutionProjectTest(BaseProjectTest):
                 self.assertTrue(job.isfile('world.txt'))
             else:
                 self.assertFalse(job.isfile('world.txt'))
+
+    def test_run_with_operation_selection(self):
+        project = self.mock_project()
+        even_jobs = [job for job in project if job.sp.b % 2 == 0]
+        with add_cwd_to_environment_pythonpath():
+            with switch_to_directory(project.root_directory()):
+                with self.assertRaises(ValueError):
+                    # The names argument must be a sequence of strings, not a string.
+                    project.run(names='op1')
+                project.run(names=['non-existent-op'])
+                self.assertFalse(any(job.isfile('world.txt') for job in even_jobs))
+                self.assertFalse(any(job.doc.get('test') for job in project))
+                project.run(names=['op1', 'non-existent-op'])
+                self.assertTrue(all(job.isfile('world.txt') for job in even_jobs))
+                self.assertFalse(any(job.doc.get('test') for job in project))
+                project.run(names=['op[^3]', 'non-existent-op'])
+                self.assertTrue(all(job.isfile('world.txt') for job in even_jobs))
+                self.assertTrue(all(job.doc.get('test') for job in project))
+                self.assertTrue(all('dynamic' not in job.doc for job in project))
 
     def test_run_parallel(self):
         project = self.mock_project()

commit 6ac773093e63a15f98b1e415080fe49a6bc57fa8

Fix issue where active operations are resubmitted.
Operations are resubmitted although already active on the scheduler.

This fix patches a bug introduced with pull request #89.

flow/project.py
@@ -2436,7 +2436,8 @@ class FlowProject(six.with_metaclass(_FlowProjectClass,
 
         # Gather all pending operations ...
         with self._potentially_buffered():
-            ops = self._get_pending_operations(jobs, args.operation_name)
+            ops = (op for op in self._get_pending_operations(jobs, args.operation_name)
+                   if self.eligible_for_submission(op))
             ops = list(islice(ops, args.num))
 
         # Bundle operations up, generate the script, and submit to scheduler.
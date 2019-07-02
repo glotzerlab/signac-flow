76ca5ce425639efaf7daa4b637ac0bc5c1dfffa6
Brandon Butler <butlerbr@umich.edu>
2019-07-01 20:51:10 -0400

    Adding after condition for groups.

--- flow/project.py
+++ flow/project.py
@@ -2553,8 +2553,10 @@ class FlowProject(six.with_metaclass(_FlowProjectClass,
                 func._flow_group.append(add_to_group.group_name)
             else:
                 func._flow_group = [add_to_group.group_name]
+            add_to_group._flow_post.extend([pc for pc in func._flow_post])
             return func
         # Specify group name
+        add_to_group._flow_post = []
         add_to_group.group_name = name
         return add_to_group
 

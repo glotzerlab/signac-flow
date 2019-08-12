glotzerlab/signac-flow

commit 0b542b40ec01f1c0ef5dd9fb4945b06470f51fa8

Issue 67 use jinja2 templating to print status (#111)
Use Jinja2 templates for the formatting of the status view.

contributors.yaml
@@ -50,7 +50,7 @@ contributors:
     affiliation: "University of Michigan"
     orcid: "https://orcid.org/0000-0002-5709-7259"
   -
-    family-names: Cersonsky 
+    family-names: Cersonsky
     given-names: Rose
     affiliation: "University of Michigan"
     orcid: "http://orcid.org/0000-0003-4515-3441"
@@ -72,6 +72,11 @@ contributors:
     family-names: Melle
     given-names: Yannah
     affiliation: "University of Michigan"
+  -
+    family-names: Zhou
+    given-names: Pengji
+    affiliation: "University of Michigan"
+    orcid: "https://orcid.org/0000-0002-1409-9633"
   -
     family-names: Justin
     given-names: Gilmer

flow/templates/base_status.jinja
@@ -1,0 +1,56 @@
+{# The following variables are available to all scripts. #}
+{% if overview %}
+{% set field_progress_bar = ("%%-%ss  %%-%ss " | format(column_width_label, column_width_bar)) %}
+{% endif %}
+{% if detailed %}
+{% set column_width_operation = [column_width_operation, 5] | max %}
+{% set field_job_id = ("%%-%ss  " | format(column_width_id)) %}
+{% set field_operation = ("%%-%ss %%3s  " | format(column_width_operation)) %}
+{% set field_operation_title = ("%%-%ss  " | format(column_width_operation+4)) %}
+{% set field_label = ("%%-%ss" | format(column_width_total_label)) %}
+{% set ns = namespace(dash='', field_parameters='') %}
+{% if parameters %}
+{% for para in parameters %}
+{% set ns.dash = ns.dash + ("%s  " | format('-' * column_width_parameters[loop.index0])) %}
+{% set ns.field_parameters = ns.field_parameters + ("%%-%ss  " | format(column_width_parameters[loop.index0])) %}
+{% endfor %}
+{% set para_head = ns.field_parameters | format(*parameters) %}
+{% endif %}
+{% set field_head = field_job_id + field_operation_title + '%s' + field_label %}
+{% endif %}
+
+{% block overview %}
+{% if overview %}
+{{ '# Overview: ' }}
+{{ 'Total # of jobs: %s ' | format(jobs | length) }}
+
+{% block progress %}
+{{ field_progress_bar | format('label', 'ratio') }}
+{{ field_progress_bar | format('-' * column_width_label, '-' * column_width_bar) }}
+{% for label in progress_sorted %}
+{{ field_progress_bar | format(label[0], label[1] | draw_progressbar(jobs | length)) }}
+{% endfor %}
+{% endblock%}
+{% endif %}
+{% endblock%}
+
+{% block detailed %}
+{% if detailed %}
+{{ '# Detailed View: ' }}
+{{ field_head | format('job_id', 'operation', para_head, 'labels') }}
+{{ field_head | format('-' * column_width_id, '-' * (column_width_operation+4), ns.dash, '-' * column_width_total_label) }}
+{% for job in jobs %}
+{% if parameters %}
+{% set para_output = ns.field_parameters | format(*job['parameters'].values()) %}
+{% endif %}
+{% for key, value in job['operations'].items() if value | job_filter(scheduler_status_code, all_ops) %}
+{% if loop.first %}
+{{ field_job_id | format(job['job_id']) }}{{ field_operation | highlight(value['eligible']) | format(key, '['+scheduler_status_code[value['scheduler_status']]+']') }}{{para_output}}{{ '%s' | format(job['labels'] | join(', ')) }}
+{% else %}
+{{ field_job_id | format('')}}{{ field_operation | highlight(value['eligible']) | format(key, '['+scheduler_status_code[value['scheduler_status']]+']') }}{{para_output}}{{ '%s' | format(job['labels'] | join(', ')) }}
+{% endif %}
+{% endfor %}
+{% endfor %}
+{{ status_legend }}
+{% endif %}
+{% endblock %}

flow/templates/base_status_compact.jinja
@@ -1,0 +1,29 @@
+{% extends "base_status.jinja" %}
+{% block detailed %}
+{% set field_operation = ("%%-%ss %%3s %%-%ss  " | format(column_width_operation, column_width_operations_count)) %}
+{% set field_operation_title = ("%%-%ss  " | format(column_width_operation+column_width_operations_count+5)) %}
+{% set field_head = field_job_id + field_operation_title + '%s' + field_label %}
+# Detailed View:
+{{ field_head | format('job_id', 'operation', para_head, 'labels') }}
+{{ field_head | format('-' * column_width_id, '-' * (column_width_operation+column_width_operations_count+5), ns.dash, '-' * column_width_total_label) }}
+{% for job in jobs %}
+{% if parameters %}
+{% set para_output = ns.field_parameters | format(*job['parameters'].values()) %}
+{% endif %}
+{% if all_ops %}
+{% set key, value = job['operations'].items() | first() %}
+{{ field_job_id | format(job['job_id']) }}{{ field_operation | highlight(value['eligible']) | format(key, '[' + scheduler_status_code[value['scheduler_status']] + ']', '+(' + extra_num_operations | string() + ')') }}{{para_output}}{{ '%s' | format(job['labels'] | join(', ')) }}
+{% else %}
+{% set ns.extra_num_operation = -1 %}
+{% set ns.if_first_eligible_operation = True %}
+{% for key, value in job['operations'].items() if value | job_filter(scheduler_status_code, all_ops) %}
+{% set ns.extra_num_operation = ns.extra_num_operation + 1 %}
+{% if loop.first %}
+{% set ns.first_operation_key = key %}
+{% set ns.first_operation_value = value %}
+{% endif %}
+{% endfor %}
+{{ field_job_id | format(job['job_id']) }}{{ field_operation | highlight(ns.first_operation_value['eligible']) | format(ns.first_operation_key, '['+scheduler_status_code[ns.first_operation_value['scheduler_status']]+']', '+('+ns.extra_num_operation | string()+')') }}{{para_output}}{{ '%s' | format(job['labels'] | join(', ')) }}
+{% endif %}
+{% endfor %}
+{% endblock %}

flow/templates/base_status_expand.jinja
@@ -1,0 +1,24 @@
+{% if compact %}
+{% extends "base_status_compact.jinja" %}
+{% elif not unroll %}
+{% extends "base_status_stack.jinja" %}
+{% else %}
+{% extends "base_status.jinja" %}
+{% endif %}
+{% block detailed %}
+{{ super () }}
+## Operations:
+{% set column_width_operation = [column_width_operation, 9] | max %}
+{% set field_operation = ("%%-%ss  " | format(column_width_operation)) %}
+{% set field_op_table = field_job_id + field_operation + '%-8s  ' + '%-14s  ' %}
+{{ field_op_table | format('job_id', 'operation', 'eligible', 'cluster_status') }}
+{{ field_op_table | format('-' * column_width_id, '-' * column_width_operation, '-' * 8, '-' * 14) }}
+{% for job in jobs %}
+{% for key, value in job['operations'].items() %}
+{% if (scheduler_status_code[value['scheduler_status']] != 'U' or value['eligible']) or all_ops%}
+{{ field_job_id | format(job['job_id']) }}{{ field_operation | highlight(value['eligible']) | format(key) }}{{ '%-8s  ' | format(alias_bool[value['eligible']])}}{{ '%-14s  ' | format(scheduler_status_code[value['scheduler_status']])}}
+{% endif %}
+{% endfor %}
+{% endfor %}
+{{ status_legend }}
+{% endblock %}

flow/templates/base_status_stack.jinja
@@ -1,0 +1,16 @@
+{% extends "base_status.jinja" %}
+{% block detailed %}
+{% set field_head = field_job_id  + '%s' + field_label %}
+{% set field_operation = ("%%1s %%-%ss %%3s  " | format(column_width_operation)) %}
+{% for job in jobs %}
+{% if parameters %}
+{% set para_output = ns.field_parameters | format(*job['parameters'].values()) %}
+{% endif %}
+{{ field_job_id | format(job['job_id']) }}{{para_output}}{{ '%s' | format(job['labels'] | join(', ')) }}
+{% for key, value in job['operations'].items() if value | job_filter(scheduler_status_code, all_ops) %}
+{{ field_operation | highlight(value['eligible']) | format(value | get_operation_status(operation_status_symbols), key, '['+scheduler_status_code[value['scheduler_status']]+']') }}
+{% endfor %}
+{% endfor %}
+{{ operation_status_legend }}
+{{ status_legend }}
+{% endblock %}

flow/templates/status.jinja
@@ -1,0 +1,1 @@
+{% extends "base_status.jinja" %}

flow/templates/status_compact.jinja
@@ -1,0 +1,1 @@
+{% extends "base_status_compact.jinja" %}

commit 394659bb554e9e68aba8ba63f0994e6a24841c26

Fix/issue 127 status view (#128)
* Add dummy job-operation to fix issue #127

Per offline discussion with @csadorf, this commit fixes creates a dummy
job operation if a job has exactly 0 eligible operations to fix a bug
that was introduced in PR #111; the bug causes the default detailed
status view to not show jobs with no eligible operations, which changes
the previous behavior.

* Update Timothy to "Timothy C." on contributors

contributors.yaml
@@ -46,7 +46,7 @@ contributors:
     orcid: "https://orcid.org/0000-0003-1852-3849"
   -
     family-names: Moore
-    given-names: Timothy
+    given-names: "Timothy C."
     affiliation: "University of Michigan"
     orcid: "https://orcid.org/0000-0002-5709-7259"
   -

flow/project.py
@@ -1418,6 +1418,17 @@ class FlowProject(six.with_metaclass(_FlowProjectClass,
                 context['operation_status_legend'] = operation_status_legend
                 context['operation_status_symbols'] = OPERATION_STATUS_SYMBOLS
 
+        def _add_dummy_operation(job):
+            job['operations'][''] = {
+                    'completed': False,
+                    'eligible': True,
+                    'scheduler_status': JobStatus.dummy}
+
+        for job in context['jobs']:
+            has_eligible_ops = any([v['eligible'] for v in job['operations'].values()])
+            if not has_eligible_ops and not context['all_ops']:
+                _add_dummy_operation(job)
+
         print(template.render(**context), file=file)
 
         # Show profiling results (if enabled)
@@ -2878,6 +2889,7 @@ _FMT_SCHEDULER_STATUS = {
     JobStatus.queued: 'Q',
     JobStatus.active: 'A',
     JobStatus.error: 'E',
+    JobStatus.dummy: ' ',
 }
 
 

flow/scheduling/base.py
@@ -16,6 +16,7 @@ class JobStatus(enum.IntEnum):
     queued = 6
     active = 7
     error = 8
+    dummy = 127
     # All user statuses are >= 128.
     user = 128
 

flow/templates/project_testing.pyt
@@ -13,11 +13,6 @@ def setup_done(job):
         return 'setup'
 
 
-@{{ project_class_name }}.label
-def op2_count(job):
-    return 'op2 {}x'.format(job.doc.get('n_op2', 0))
-
-
 @{{ project_class_name }}.operation
 @{{ project_class_name }}.post(setup_done)
 def setup(job):
@@ -26,7 +21,7 @@ def setup(job):
 
 
 @{{ project_class_name }}.operation
-@{{ project_class_name }}.pre(lambda job: job.sp.get('b') is False)
+@{{ project_class_name }}.pre(lambda job: job.sp.get('a'))
 @{{ project_class_name }}.pre.after(setup)
 @{{ project_class_name }}.post.isfile('op1.txt')
 def op1(job):
@@ -34,7 +29,13 @@ def op1(job):
         f.write('b: False')
 
 
+@{{ project_class_name }}.label
+def op2_count(job):
+    return 'op2({}x)'.format(job.doc.get('n_op2', 0))
+
+
 @{{ project_class_name }}.operation
+@{{ project_class_name }}.pre.after(setup)
 @{{ project_class_name }}.post(lambda job: job.doc.get('n_op2', 0) >= 3)
 def op2(job):
     with open(job.fn('op2.txt'), 'a') as f:
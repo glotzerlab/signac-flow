c3871ff1344c8934092e30c72755c5f1993b0711
Pengji <zhoupj@umich.edu>
2019-08-30 13:24:39 -0400

    add depreciation notice for tabulate.py (#147)
    Remove tabulate module (replaced by Jinja for printing status).

--- flow/util/__init__.py
+++ flow/util/__init__.py
@@ -2,6 +2,6 @@
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 "Defines the API for the util sub package."
-from . import tabulate, tqdm, misc, translate, template_filters
+from . import tqdm, misc, translate, template_filters
 
-__all__ = ['tabulate', 'tqdm', 'misc', 'translate', 'template_filters']
+__all__ = ['tqdm', 'misc', 'translate', 'template_filters']

40f79a2ba45c06b140de0b1d30c0a21a4fb96287
Pengji Zhou <zhoupj@umich.edu>
2019-11-27 12:54:58 -0500

    remove redundent template in summit.sh

--- flow/templates/summit.sh
+++ flow/templates/summit.sh
@@ -12,17 +12,3 @@
 #BSUB -P {{ account }}
 {% endif %}
 {% endblock %}
-{% block body %}
-{% set cmd_suffix = cmd_suffix|default('') ~ (' &' if parallel else '') %}
-{% for operation in operations %}
-{% if not mpi_prefix %}
-{% set mpi_prefix = operation|get_mpi_prefix %}
-{% endif %}
-
-# {{ "%s"|format(operation) }}
-{% if operation.directives.omp_num_threads %}
-export OMP_NUM_THREADS={{ operation.directives.omp_num_threads }}
-{% endif %}
-{{ mpi_prefix }}{{ cmd_prefix }}{{ operation.cmd }}{{ cmd_suffix }}
-{% endfor %}
-{% endblock %}

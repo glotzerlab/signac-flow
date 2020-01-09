ea7caf0f247a93031dadf1e04be1549c491c7ef7
Bradley Dice <bdice@bradleydice.com>
2020-01-09 17:40:56 -0500

    Bump up to version 0.9.0.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='signac-flow',
-    version='0.8.0',
+    version='0.9.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,

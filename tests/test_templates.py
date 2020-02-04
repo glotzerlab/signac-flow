c4f79900ee07f1f86a618ba0f216ee06fa729e80
kidrahahjo <kidrahahjo@gmail.com>
2020-02-04 19:27:06 +0530

    pytest implementation used

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -15,79 +15,76 @@ import generate_template_reference_data as gen
 from test_project import redirect_stdout, redirect_stderr
 
 
-class TestTemplateBase(object):
-    project_class = signac.Project
-    env = None
+project_class = signac.Project
 
-    def _env_name(self):
-        name = '{}.{}'.format(self.env.__module__, self.env.__name__)
-        return '.'.join(name.split('.')[1:])
+def _env_name(env):
+    name = '{}.{}'.format(env.__module__, env.__name__)
+    return '.'.join(name.split('.')[1:])
 
-    def find_envs():
-        for name, env in flow.environment.ComputeEnvironment.registry.items():
-            if env.__module__.startswith('flow.environments'):
-                yield env
+def find_envs():
+    """Yields the environments to be tested."""
+    for name, env in flow.environment.ComputeEnvironment.registry.items():
+        if env.__module__.startswith('flow.environments'):
+            yield env
 
-    @pytest.mark.parametrize('env', find_envs())
-    def test_env(self, env):
-        # Force asserts to show the full file when failures occur.
-        # Useful to debug errors that arise.
-        self.maxDiff = None
-        self.env = env
+@pytest.mark.parametrize('env', find_envs())
+def test_env(env):
+    # Force asserts to show the full file when failures occur.
+    # Useful to debug errors that arise.
 
-        # Must import the data into the project.
-        with signac.TemporaryProject(name=gen.PROJECT_NAME) as p:
-            fp = gen.get_masked_flowproject(p)
-            fp.import_from(origin=gen.ARCHIVE_DIR)
-            jobs = fp.find_jobs(dict(environment=self._env_name()))
-            if not len(jobs):
-                raise RuntimeError(
-                    "No reference data for environment {}!".format(self._env_name())
-                    )
-            reference = []
-            generated = []
-            for job in jobs:
-                parameters = job.sp.parameters()
-                if 'bundle' in parameters:
-                    bundle = parameters.pop('bundle')
+    # Must import the data into the project.
+    with signac.TemporaryProject(name=gen.PROJECT_NAME) as p:
+        fp = gen.get_masked_flowproject(p)
+        fp.import_from(origin=gen.ARCHIVE_DIR)
+        jobs = fp.find_jobs(dict(environment=_env_name(env)))
+        if not len(jobs):
+            raise RuntimeError(
+                "No reference data for environment {}!".format(_env_name(env))
+                )
+        reference = []
+        generated = []
+        for job in jobs:
+            parameters = job.sp.parameters()
+            if 'bundle' in parameters:
+                bundle = parameters.pop('bundle')
+                tmp_out = io.TextIOWrapper(
+                    io.BytesIO(), sys.stdout.encoding)
+                with open(os.devnull, 'w') as devnull:
+                    with redirect_stderr(devnull):
+                        with redirect_stdout(tmp_out):
+                            fp.submit(
+                                env=env, jobs=[job], names=bundle, pretend=True,
+                                force=True, bundle_size=len(bundle), **parameters)
+                tmp_out.seek(0)
+                msg = "---------- Bundled submission of job {}".format(job)
+                generated.extend([msg] + tmp_out.read().splitlines())
+
+                with open(job.fn('script_{}.sh'.format('_'.join(bundle)))) as file:
+                    reference.extend([msg] + file.read().splitlines())
+            else:
+                for op in fp.operations:
+                    if 'partition' in parameters:
+                        # Don't try to submit GPU operations to CPU partitions
+                        # and vice versa.  We should be able to relax this
+                        # requirement if we make our error checking more
+                        # consistent.
+                        if operator.xor(
+                            'gpu' in parameters['partition'].lower(),
+                                'gpu' in op.lower()):
+                            continue
                     tmp_out = io.TextIOWrapper(
                         io.BytesIO(), sys.stdout.encoding)
                     with open(os.devnull, 'w') as devnull:
                         with redirect_stderr(devnull):
                             with redirect_stdout(tmp_out):
                                 fp.submit(
-                                    env=env, jobs=[job], names=bundle, pretend=True,
-                                    force=True, bundle_size=len(bundle), **parameters)
+                                    env=env, jobs=[job],
+                                    names=[op], pretend=True, force=True, **parameters)
                     tmp_out.seek(0)
-                    msg = "---------- Bundled submission of job {}".format(job)
+                    msg = "---------- Submission of operation {} for job {}.".format(op, job)
                     generated.extend([msg] + tmp_out.read().splitlines())
 
-                    with open(job.fn('script_{}.sh'.format('_'.join(bundle)))) as file:
+                    with open(job.fn('script_{}.sh'.format(op))) as file:
                         reference.extend([msg] + file.read().splitlines())
-                else:
-                    for op in fp.operations:
-                        if 'partition' in parameters:
-                            # Don't try to submit GPU operations to CPU partitions
-                            # and vice versa.  We should be able to relax this
-                            # requirement if we make our error checking more
-                            # consistent.
-                            if operator.xor(
-                                'gpu' in parameters['partition'].lower(),
-                                    'gpu' in op.lower()):
-                                continue
-                        tmp_out = io.TextIOWrapper(
-                            io.BytesIO(), sys.stdout.encoding)
-                        with open(os.devnull, 'w') as devnull:
-                            with redirect_stderr(devnull):
-                                with redirect_stdout(tmp_out):
-                                    fp.submit(
-                                        env=env, jobs=[job],
-                                        names=[op], pretend=True, force=True, **parameters)
-                        tmp_out.seek(0)
-                        msg = "---------- Submission of operation {} for job {}.".format(op, job)
-                        generated.extend([msg] + tmp_out.read().splitlines())
 
-                        with open(job.fn('script_{}.sh'.format(op))) as file:
-                            reference.extend([msg] + file.read().splitlines())
-
-            assert '\n'.join(reference) == '\n'.join(generated)
+        assert '\n'.join(reference) == '\n'.join(generated)

679495d4793fb89078283341416fd19de7de2db5
ac-optimus <abhavya.chandra@iitgn.ac.in>
2020-02-23 10:07:13 +0530

    adding ValueError for post condition and improving the readability of cod

--- flow/project.py
+++ flow/project.py
@@ -557,9 +557,11 @@ class _FlowProjectClass(type):
                 super(pre, self).__init__(condition, tag)
 
             def __call__(self, func):
-                for (name, func_) in self._parent_class._collect_operations():
-                    if self.condition is func_:
-                        raise ValueError("Operation functions cannot be used as preconditions.")
+                operation_functions = []
+                for operation in self._parent_class._collect_operations():
+                    operation_functions.append(operation[1])
+                if self.condition in operation_functions:
+                    raise ValueError("Operation functions cannot be used as preconditions.")
                 self._parent_class._OPERATION_PRE_CONDITIONS[func].insert(0, self.condition)
                 return func
 
@@ -572,13 +574,11 @@ class _FlowProjectClass(type):
             @classmethod
             def after(cls, *other_funcs):
                 "True if and only if all post conditions of other operation-function(s) are met."
-                for condition_i in other_funcs:
-                    check_flag = 0
-                    for (name, func_) in cls._parent_class._collect_operations():
-                        if condition_i is func_:
-                            check_flag = 1
-                    if check_flag != 1:
-                        raise ValueError("The argument to pre.after must be an operation function.")
+                operation_functions = []
+                for operation in cls._parent_class._collect_operations():
+                    operation_functions.append(operation[1])
+                if not all(condition in operation_functions for condition in other_funcs):
+                    raise ValueError("The arguments to pre.after must be operation functions.")
                 return cls(_create_all_metacondition(cls._parent_class._collect_post_conditions(),
                                                      *other_funcs))
 
@@ -613,6 +613,11 @@ class _FlowProjectClass(type):
                 super(post, self).__init__(condition, tag)
 
             def __call__(self, func):
+                operation_functions = []
+                for operation in self._parent_class._collect_operations():
+                    operation_functions.append(operation[1])
+                if self.condition in operation_functions:
+                    raise ValueError("Operation functions cannot be used as postconditions.")
                 self._parent_class._OPERATION_POST_CONDITIONS[func].insert(0, self.condition)
                 return func
 

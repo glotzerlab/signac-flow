ce0df5321b50d9b2c9b02348279302c792b45e9c
Vyas Ramasubramani <vramasub@umich.edu>
2020-02-27 14:07:23 -0500

    Remove unused function

--- flow/project.py
+++ flow/project.py
@@ -1497,15 +1497,6 @@ class FlowProject(signac.contrib.Project, metaclass=_FlowProjectClass):
         for sjob in self._expand_bundled_jobs(scheduler.jobs()):
             yield sjob
 
-    @staticmethod
-    def _map_scheduler_jobs(scheduler_jobs):
-        "Map all scheduler jobs by job id and operation name."
-        for sjob in scheduler_jobs:
-            name = sjob.name()
-            if name[32] == '-':
-                expanded = JobOperation.expand_id(name)
-                yield expanded['job_id'], expanded['operation-name'], sjob
-
     def _get_operations_status(self, job, cached_status):
         "Return a dict with information about job-operations for this job."
         starting_dict = functools.partial(dict, scheduler_status=JobStatus.unknown)

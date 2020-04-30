1ddc75f24af5719075620bef1292ed07d3012ac3
Hardik Ojha <44747868+kidrahahjo@users.noreply.github.com>
2020-04-30 18:58:17 +0530

    _DirectivesItem tests refactored

--- tests/test_directives.py
+++ tests/test_directives.py
@@ -3,11 +3,11 @@
 # This software is licensed under the BSD 3-Clause License.
 import pytest
 import sys
-from copy import deepcopy
 
 from flow.directives import Directives, _DirectivesItem
 from flow.directives import NP, NRANKS, NGPU, EXECUTABLE, OMP_NUM_THREADS
 from flow.directives import WALLTIME, MEMORY, PROCESS_FRACTION
+from flow.directives import no_aggregation
 
 
 @pytest.fixture()
@@ -16,11 +16,33 @@ def setUp(test_class=Directives, **kwargs):
         return test_class(**kwargs)
     return _setup
 
+
 @pytest.fixture()
 def available_directives_list():
     return [NP, NRANKS, NGPU, OMP_NUM_THREADS,
             EXECUTABLE, WALLTIME, MEMORY,
             PROCESS_FRACTION]
+
+
+@pytest.fixture()
+def product_directive():
+    def val(v):
+        if v < 10:
+            raise ValueError("Price cannot be less than 10 units")
+        return v
+
+    def finalize(value, dict):
+        discount = dict.get('discount', 0)
+        free = dict.get('free', False)
+        value = value - discount
+        if value < 0 or free:
+            return 0
+        return value
+
+    product = _DirectivesItem(name='product', validation=val,
+                              default=10, serial=no_aggregation,
+                              parallel=no_aggregation, finalize=finalize)
+    return product
 
 
 class TestItems:
@@ -37,57 +59,29 @@ class TestItems:
         assert MEMORY.default == 4
         assert PROCESS_FRACTION.default == 1.
 
-    def test_validation(self, available_directives_list):
+    def test_defaults_are_valid(self, available_directives_list):
         for directive in available_directives_list:
             directive.validation(directive.default)
-
-    # def test_validation_none(self):
-    #     available_directives_list =
-    #     for directive in available_directives_list:
-    #         directive.validation(directive.default)
-
-    def test_validation_manual(self):
-        directive_copy = deepcopy(NP)
-
-        def div_by_ten(v):
-            if v % 10 == 0:
-                return True
-            return False
-
-        directive_copy.validation = div_by_ten
-        assert directive_copy.validation(10)
-        assert not directive_copy.validation(5)
-        with pytest.raises(TypeError):
-            directive_copy.validation('4')
 
     def test_serial(self):
         assert NP.serial(4, 2) == 4
         assert NRANKS.serial(4, 2) == 4
         assert NGPU.serial(4, 2) == 4
-        assert OMP_NUM_THREADS(4, 2) == 4
+        assert OMP_NUM_THREADS.serial(4, 2) == 4
         assert EXECUTABLE.serial('Path1', 'Path2') == 'Path1'
         assert WALLTIME.serial(4, 2) == 6
         assert MEMORY.serial(4, 2) == 4
-        assert PROCESS_FRACTION(4.0, 2.0) == 4.0
+        assert PROCESS_FRACTION.serial(0.4, 0.2) == 0.4
 
     def test_parallel(self):
         assert NP.parallel(4, 2) == 6
         assert NRANKS.parallel(4, 2) == 6
         assert NGPU.parallel(4, 2) == 6
-        assert OMP_NUM_THREADS(4, 2) == 6
+        assert OMP_NUM_THREADS.parallel(4, 2) == 6
         assert EXECUTABLE.parallel('Path1', 'Path2') == 'Path1'
         assert WALLTIME.parallel(4, 2) == 4
         assert MEMORY.parallel(4, 2) == 6
-        assert PROCESS_FRACTION.parallel(4.0, 2.0) == 4.0
-
-    def test_parallel_manual(self):
-        directive_copy = deepcopy(NP)
-        directive_copy.parallel = lambda x, y: x*y
-        parallel = directive_copy.parallel(3, 2)
-        assert parallel == 6
-        directive_copy.validation(parallel)
-        with pytest.raises(TypeError):
-            directive_copy.parallel('4', '2')
+        assert PROCESS_FRACTION.parallel(0.4, 0.2) == 0.4
 
     def test_finalize(self):
         dict_directives = {'nranks': NRANKS.default, 'omp_num_threads': OMP_NUM_THREADS.default}
@@ -98,38 +92,31 @@ class TestItems:
         dict_directives['nranks'] = NRANKS
         assert NP.finalize(2, dict_directives) == 2
 
-    def test_manual_item(self, setUp):
-        def val(v):
-            if v < 10:
-                raise ValueError("Price cannot be less than 10 units")
-            return v
+    def test_manual_item_default(self, product_directive):
+        assert product_directive.default == 10
 
-        def no_aggregation(v, o):
-            return v
-
-        def finalize(value, dict):
-            discount = dict.get('discount', 0)
-            free = dict.get('free', False)
-            value = value - discount
-            if value < 0 or free:
-                return 0
-            return value
-
-        PRODUCT = setUp(_DirectivesItem, name='product', validation=val,
-                        default=10, serial=no_aggregation, parallel=no_aggregation,
-                        finalize=finalize)
-        asset_dict = {'free': False, 'discount': 5}
-        assert PRODUCT.default == PRODUCT.validation(PRODUCT.default)
-        assert PRODUCT.default == PRODUCT.validation(20)
-        assert PRODUCT.default == 10
-        assert PRODUCT.serial(10, 20) == 10
-        assert PRODUCT.parallel(10, 20) == 10
-        assert PRODUCT.finalize(50, asset_dict) == 45
-        asset_dict['free'] = True
-        assert PRODUCT.finalize(50, asset_dict) == 0
-
+    def test_manual_item_validation(self, product_directive):
+        try:
+            val = product_directive.validation(product_directive.default)
+            assert product_directive.default == val
+        except Exception:
+            raise ValueError("Invalid default value for product_directive")
+        assert product_directive.default == product_directive.validation(product_directive.default)
+        assert product_directive.validation(20) == 20
         with pytest.raises(ValueError):
-            PRODUCT.validation(0)
+            product_directive.validation(0)
+
+    def test_manual_item_serial(self, product_directive):
+        product_directive.serial(10, 20) == 10
+
+    def test_manual_item_parallel(self, product_directive):
+        product_directive.parallel(10, 20) == 10
+
+    def test_manual_item_finalize(self, product_directive):
+        asset_dict = {'free': False, 'discount': 5}
+        assert product_directive.finalize(50, asset_dict) == 45
+        asset_dict['free'] = True
+        assert product_directive.finalize(50, asset_dict) == 0
 
 
 class TestDirectives:

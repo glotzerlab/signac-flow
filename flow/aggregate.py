417ff18e37dc693e6b048531f8d378ebe88f6eec
Hardik Ojha <44747868+kidrahahjo@users.noreply.github.com>
2020-07-16 21:24:01 +0530

    Suggested changes made

--- flow/aggregate.py
+++ flow/aggregate.py
@@ -2,20 +2,16 @@
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 from collections.abc import Iterable
-from copy import deepcopy
 from itertools import groupby
 from itertools import zip_longest
 from functools import partial
 
-import signac
-from signac.contrib.project import Project, JobsCursor
 
-
-class aggregate:
+class Aggregate:
     """Decorator for operation functions that are to be aggregated.
 
-    If this class is used for aggregation then by-default, if the aggregator
-    parameter is not passed, an aggregate of all jobs will be created.
+    By default, if the aggregator parameter is not passed,
+    an aggregate of all jobs will be created.
 
     .. code-block:: python
 
@@ -26,14 +22,15 @@ class aggregate:
             print(len(jobs))
 
     :param aggregator:
-        Information on how to aggregate jobs. Takes in a list of
-        jobs and can return or yield lists or single jobs instead.
-        The default behavior is creating an aggregate of all jobs.
+        Information describing how to aggregate jobs. Is a callable that
+        takes in a list of jobs and can return or yield subsets of jobs as
+        an iterable. The default behavior is creating a single aggregate
+        of all jobs
     :type aggregator:
         callable
     :param sort:
-        Before aggregating, sort the jobs given by a statepoint parameter.
-        The default value is None.
+        Before aggregating, sort the jobs by a given statepoint parameter.
+        The default behaviour is no sorting.
     :type sort:
         str or NoneType
     :param reverse:
@@ -42,8 +39,9 @@ class aggregate:
     :type reverse:
         bool
     :param select:
-        Condition for filtering jobs. This operates on a single job.
-        The default value is None
+        Condition for filtering individual jobs. This is passed as the
+        callable argument to `filter`.
+        The default behaviour is no filtering.
     :type select:
         callable or NoneType
     """
@@ -51,25 +49,24 @@ class aggregate:
     def __init__(self, aggregator=None, sort=None, reverse=False, select=None):
         if aggregator is None:
             def aggregator(jobs):
-                return jobs
+                return [jobs]
 
         if not callable(aggregator):
-            raise TypeError("Expected callable aggregator function, got {}"
+            raise TypeError("Expected callable for aggregator, got {}"
                             "".format(type(aggregator)))
 
         if sort is not None and not isinstance(sort, str):
-            raise TypeError("Expected string sort parameter, got {}".format(type(sort)))
-
-        if not isinstance(reverse, bool):
-            raise TypeError("Expected bool reverse parameter got {}".format(type(reverse)))
+            raise TypeError("Expected string sort parameter, got {}"
+                            "".format(type(sort)))
 
         if select is not None and not callable(select):
-            raise TypeError("Expected callable select parameter, got {}".format(type(select)))
+            raise TypeError("Expected callable for select, got {}"
+                            "".format(type(select)))
 
         self._aggregator = aggregator
         self._sort = None if sort is None else partial(sorted,
                                                        key=lambda job: job.sp[sort],
-                                                       reverse=reverse)
+                                                       reverse=bool(reverse))
         self._select = None if select is None else partial(filter, select)
 
     @classmethod
@@ -105,8 +102,16 @@ class aggregate:
                 def keyfunction(job):
                     return [job.sp[key] for key in keys]
             else:
+                if isinstance(default, Iterable):
+                    if len(default) != len(keys):
+                        raise ValueError("Expected length of default argument is {}, "
+                                         "got {}.".format(len(keys), len(default)))
+                else:
+                    raise TypeError("Invalid default argument. Expected Iterable, "
+                                    "got {}".format(type(default)))
+
                 def keyfunction(job):
-                    return [job.sp.get(key, default) for key in keys]
+                    return [job.sp.get(key, default[i]) for i, key in enumerate(keys)]
 
         elif callable(key):
             keyfunction = key
@@ -121,63 +126,56 @@ class aggregate:
 
         return cls(aggregator, sort, reverse, select)
 
-    def __call__(self, obj=None):
-        if callable(obj):
-            setattr(obj, '_flow_aggregate', self)
-            return obj
-        elif isinstance(obj, (list, Project, JobsCursor)):
-            aggregated_jobs = deepcopy(list(obj))
-            if self._select is not None:
-                aggregated_jobs = list(self._select(aggregated_jobs))
-            if self._sort is not None:
-                aggregated_jobs = list(self._sort(aggregated_jobs))
-            aggregated_jobs = self._aggregator([job for job in aggregated_jobs])
-            aggregated_jobs = self._create_nested_aggregate_list(aggregated_jobs)
-            if not len(aggregated_jobs):
-                return []
-            for i, job in enumerate(aggregated_jobs[-1]):
-                if job is None:
-                    del aggregated_jobs[-1][i:]
-                    break
-            return aggregated_jobs
-        elif isinstance(obj, signac.contrib.job.Job):
-            # For backwards compatibility
-            return [obj]
+    def __call__(self, func=None):
+        if callable(func):
+            setattr(func, '_flow_aggregate', _MakeAggregate(self))
+            return func
         else:
             raise TypeError('Invalid argument passed while calling '
-                            'the aggregate instance. Expected a callable '
-                            'or an Iterable of Jobs, got {}.'.format(type(obj)))
+                            'the aggregate instance. Expected a callable, '
+                            'got {}.'.format(type(func)))
+
+
+class _MakeAggregate:
+    """This class handles the creation of aggregates.
+
+    .. note::
+        This class should not be instantiated by users directly.
+
+    :param aggregate:
+        Aggregate object associated with an operation function
+    :type aggregate:
+        :py:class:`Aggregate`
+    """
+    def __init__(self, aggregate=Aggregate()):
+        self._aggregate = aggregate
+
+    def __call__(self, obj):
+        "Return aggregated jobs"
+        aggregated_jobs = list(obj)
+        if self._aggregate._select is not None:
+            aggregated_jobs = list(self._aggregate._select(aggregated_jobs))
+        if self._aggregate._sort is not None:
+            aggregated_jobs = list(self._aggregate._sort(aggregated_jobs))
+        aggregated_jobs = self._aggregate._aggregator([job for job in aggregated_jobs])
+        aggregated_jobs = self._create_nested_aggregate_list(aggregated_jobs)
+        if not len(aggregated_jobs):
+            return []
+        for i, job in enumerate(aggregated_jobs[-1]):
+            if job is None:
+                del aggregated_jobs[-1][i:]
+                break
+        return aggregated_jobs
 
     def _create_nested_aggregate_list(self, aggregated_jobs):
-        # When jobs are aggregated, the user is required to specify an aggregator
-        # function which either returns a nested list or nested iterable
-        # in which the base element is an instance of `signac.contrib.job.Job`.
-        # This method converts the returned object to a nested list.
+        # This method converts the returned subset of jobs as an Iterable
+        # from an aggregator function to a subset of jobs as list.
         aggregated_jobs = list(aggregated_jobs)
-        all_iterable = all_jobs = True
         nested_aggregates = []
         for aggregate in aggregated_jobs:
-            if isinstance(aggregate, Iterable):
-                all_jobs = False
-                if not all_iterable:
-                    # Got something like [Iterable, Iterable, Not an Iterable object]
-                    raise ValueError("Invalid aggregator function provided by "
-                                     "user.")
-                if isinstance(aggregate, (list, tuple)):
-                    nested_aggregates.append(list(aggregate))
-                else:
-                    nested_aggregates.append([job for job in aggregate])
-            elif isinstance(aggregate, signac.contrib.job.Job):
-                all_iterable = False
-                if not all_jobs:
-                    # Got something like [Job, Job, Not a Job object]
-                    raise ValueError("Invalid aggregator function provided by "
-                                     "user.")
-                continue
-            else:
+            try:
+                nested_aggregates.append([job for job in aggregate])
+            except Exception:
                 raise ValueError("Invalid aggregator function provided by "
-                                 "user.")
-        if all_iterable:
-            return nested_aggregates
-        elif all_jobs:
-            return [aggregated_jobs]
+                                 "the user.")
+        return nested_aggregates
--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -4,7 +4,7 @@ from functools import partial
 from tempfile import TemporaryDirectory
 
 import signac
-from flow.aggregate import aggregate
+from flow.aggregate import Aggregate, _MakeAggregate
 
 
 class AggregateProjectSetup:
@@ -37,61 +37,43 @@ class AggregateProjectSetup:
 class TestAggregate(AggregateProjectSetup):
 
     def test_default_init(self):
-        aggregate_instance = aggregate()
+        aggregate_instance = Aggregate()
         test_list = [1, 2, 3, 4, 5]
         assert aggregate_instance._sort is None
-        assert aggregate_instance._aggregator(test_list) == test_list
+        assert aggregate_instance._aggregator(test_list) == [test_list]
         assert aggregate_instance._select is None
 
     def test_invalid_aggregator(self, setUp, project):
         aggregators = ['str', 1, {}]
         for aggregator in aggregators:
             with pytest.raises(TypeError):
-                aggregate(aggregator)
-
-        def invalid_aggregator_function(jobs):
-            _aggregate = []
-            for job in jobs:
-                if job.sp.i > 5:
-                    _aggregate.append(job)
-                else:
-                    _aggregate.append([job])
-            return _aggregate
-
-        with pytest.raises(ValueError):
-            aggregate(invalid_aggregator_function)(project)
+                Aggregate(aggregator)
 
     def test_invalid_sort(self):
         sort_list = [1, {}, lambda x: x]
         for sort in sort_list:
             with pytest.raises(TypeError):
-                aggregate(sort=sort)
-
-    def test_invalid_reverse(self):
-        reverse_list = ['str', 1, {}, lambda x: x]
-        for reverse in reverse_list:
-            with pytest.raises(TypeError):
-                aggregate(reverse=reverse)
+                Aggregate(sort=sort)
 
     def test_invalid_select(self):
         selectors = ['str', 1, []]
         for _select in selectors:
             with pytest.raises(TypeError):
-                aggregate(select=_select)
+                Aggregate(select=_select)
 
     def test_invalid_call(self):
         call_params = ['str', 1, None]
         for param in call_params:
             with pytest.raises(TypeError):
-                aggregate()(param)
+                Aggregate()(param)
 
     def test_call_without_function(self):
-        aggregate_instance = aggregate()
+        aggregate_instance = Aggregate()
         with pytest.raises(TypeError):
             aggregate_instance()
 
     def test_call_with_function(self):
-        aggregate_instance = aggregate()
+        aggregate_instance = Aggregate()
 
         def test_function(x):
             return x
@@ -101,7 +83,7 @@ class TestAggregate(AggregateProjectSetup):
         assert getattr(test_function, '_flow_aggregate', False)
 
     def test_with_decorator_with_pre_initialization(self):
-        aggregate_instance = aggregate()
+        aggregate_instance = Aggregate()
 
         @aggregate_instance
         def test_function(x):
@@ -110,49 +92,69 @@ class TestAggregate(AggregateProjectSetup):
         assert getattr(test_function, '_flow_aggregate', False)
 
     def test_with_decorator_without_pre_initialization(self):
-        @aggregate()
+        @Aggregate()
         def test_function(x):
             return x
 
         assert getattr(test_function, '_flow_aggregate', False)
 
+    def test_groups_of_invalid_num(self):
+        invalid_values = [{}, 'str', -1, -1.5]
+        for invalid_value in invalid_values:
+            with pytest.raises((TypeError, ValueError)):
+                Aggregate.groupsof(invalid_value)
+
+    def test_group_by_invalid_key(self):
+        with pytest.raises(TypeError):
+            Aggregate.groupby(1)
+
+    def test_groupby_with_invalid_type_default_key_for_Iterable(self, setUp, project):
+        with pytest.raises(TypeError):
+            Aggregate.groupby(['half', 'even'], default=-1)
+
+    def test_groupby_with_invalid_length_default_key_for_Iterable(self, setUp, project):
+        with pytest.raises(ValueError):
+            Aggregate.groupby(['half', 'even'], default=[-1, -1, -1])
+
+
+class TestMakeAggregate(AggregateProjectSetup):
+
     def test_valid_aggregator_non_partial(self, setUp, project):
         # Return groups of 1
         def helper_aggregator(jobs):
-            yield from jobs
+            for job in jobs:
+                yield [job]
 
-        aggregate_instance = aggregate(helper_aggregator)
+        aggregate_instance = Aggregate(helper_aggregator)
 
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         aggregate_job_manual = helper_aggregator(project)
         aggregate_job_via_aggregator = aggregate_instance(project)
 
-        assert [[jobs for jobs in aggregate_job_manual]] == \
+        assert [aggregate for aggregate in aggregate_job_manual] == \
                aggregate_job_via_aggregator
 
     def test_valid_aggregator_partial(self, setUp, project):
-        aggregate_instance = aggregate(lambda jobs: jobs)
+        aggregate_instance = Aggregate(lambda jobs: [jobs])
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         aggregate_job_via_aggregator = aggregate_instance(project)
 
-        assert [[jobs for jobs in project]] == \
+        assert [[job for job in project]] == \
                aggregate_job_via_aggregator
 
     def test_valid_sort(self, setUp, project):
         helper_sort = partial(sorted, key=lambda job: job.sp.i)
-        aggregate_instance = aggregate(sort='i')
+        aggregate_instance = Aggregate(sort='i')
+        aggregate_instance = _MakeAggregate(aggregate_instance)
 
         assert([helper_sort(project)] == aggregate_instance(project))
 
     def test_valid_reversed_sort(self, setUp, project):
         helper_sort = partial(sorted, key=lambda job: job.sp.i, reverse=True)
-        aggregate_instance = aggregate(sort='i', reverse=True)
+        aggregate_instance = Aggregate(sort='i', reverse=True)
+        aggregate_instance = _MakeAggregate(aggregate_instance)
 
         assert([helper_sort(project)] == aggregate_instance(project))
-
-    def test_groups_of_invalid_num(self):
-        invalid_values = [{}, 'str', -1, -1.5]
-        for invalid_value in invalid_values:
-            with pytest.raises((TypeError, ValueError)):
-                aggregate.groupsof(invalid_value)
 
     def test_groups_of_valid_num(self, setUp, project):
         valid_values = [1, 2, 3, 6]
@@ -160,7 +162,8 @@ class TestAggregate(AggregateProjectSetup):
         total_jobs = len(project)
 
         for valid_value in valid_values:
-            aggregate_instance = aggregate.groupsof(valid_value)
+            aggregate_instance = Aggregate.groupsof(valid_value)
+            aggregate_instance = _MakeAggregate(aggregate_instance)
             aggregate_job_via_aggregator = aggregate_instance(project)
             if total_jobs % valid_value == 0:
                 length_of_aggregate = total_jobs/valid_value
@@ -168,45 +171,47 @@ class TestAggregate(AggregateProjectSetup):
                 length_of_aggregate = int(total_jobs/valid_value) + 1
             assert len(aggregate_job_via_aggregator) == length_of_aggregate
 
-    def test_group_by_invalid_key(self):
-        with pytest.raises(TypeError):
-            aggregate.groupby(1)
-
     def test_groupby_with_valid_string_key(self, setUp, project):
-        aggregate_instance = aggregate.groupby('even')
+        aggregate_instance = Aggregate.groupby('even')
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         aggregates = 0
         for agg in aggregate_instance(project):
             aggregates += 1
         assert aggregates == 2
 
     def test_groupby_with_invalid_string_key(self, setUp, project):
-        aggregate_instance = aggregate.groupby('invalid_key')
+        aggregate_instance = Aggregate.groupby('invalid_key')
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         with pytest.raises(KeyError):
             for agg in aggregate_instance(project):
                 pass
 
     def test_groupby_with_default_key_for_string(self, setUp, project):
-        aggregate_instance = aggregate.groupby('half', default=-1)
+        aggregate_instance = Aggregate.groupby('half', default=-1)
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         aggregates = 0
         for agg in aggregate_instance(project):
             aggregates += 1
         assert aggregates == 6
 
     def test_groupby_with_Iterable_key(self, setUp, project):
-        aggregate_instance = aggregate.groupby(['i', 'even'])
+        aggregate_instance = Aggregate.groupby(['i', 'even'])
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         aggregates = 0
         for agg in aggregate_instance(project):
             aggregates += 1
         assert aggregates == 10
 
     def test_groupby_with_invalid_Iterable_key(self, setUp, project):
-        aggregate_instance = aggregate.groupby(['half', 'even'])
+        aggregate_instance = Aggregate.groupby(['half', 'even'])
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         with pytest.raises(KeyError):
             for agg in aggregate_instance(project):
                 pass
 
-    def test_groupby_with_default_key_for_Iterable(self, setUp, project):
-        aggregate_instance = aggregate.groupby(['half', 'even'], default=-1)
+    def test_groupby_with_valid_default_key_for_Iterable(self, setUp, project):
+        aggregate_instance = Aggregate.groupby(['half', 'even'], default=[-1, -1])
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         aggregates = 0
         for agg in aggregate_instance(project):
             aggregates += 1
@@ -216,7 +221,8 @@ class TestAggregate(AggregateProjectSetup):
         def keyfunction(job):
             return job.sp['even']
 
-        aggregate_instance = aggregate.groupby(keyfunction)
+        aggregate_instance = Aggregate.groupby(keyfunction)
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         aggregates = 0
         for agg in aggregate_instance(project):
             aggregates += 1
@@ -225,7 +231,8 @@ class TestAggregate(AggregateProjectSetup):
     def test_groupby_with_invalid_callable_key(self, setUp, project):
         def keyfunction(job):
             return job.sp['half']
-        aggregate_instance = aggregate.groupby(keyfunction)
+        aggregate_instance = Aggregate.groupby(keyfunction)
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         with pytest.raises(KeyError):
             for agg in aggregate_instance(project):
                 pass
@@ -234,7 +241,8 @@ class TestAggregate(AggregateProjectSetup):
         def _select(job):
             return job.sp.i > 5
 
-        aggregate_instance = aggregate.groupsof(1, select=_select)
+        aggregate_instance = Aggregate.groupsof(1, select=_select)
+        aggregate_instance = _MakeAggregate(aggregate_instance)
         selected_jobs = []
         for job in project:
             if _select(job):

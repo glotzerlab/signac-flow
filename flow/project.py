7b337fea2bde5ee701effdb899234f1691c63d98
Carl Simon Adorf <csadorf@umich.edu>
2020-08-20 10:22:53 +0200

    Respect JOB_ID_SEPERATOR for bundled scheduler jobs.
    Resolves issue #343.

--- flow/project.py
+++ flow/project.py
@@ -1561,8 +1561,9 @@ class FlowProject(signac.contrib.Project, metaclass=_FlowProjectClass):
         if len(operations) == 1:
             return operations[0].id
         else:
-            h = '.'.join(op.id for op in operations)
-            bid = '{}/bundle/{}'.format(self, sha1(h.encode('utf-8')).hexdigest())
+            sep = getattr(self._environment, 'JOB_ID_SEPARATOR', '/')
+            _id = sha1('.'.join(op.id for op in operations).encode('utf-8')).hexdigest()
+            bid = "{pr}{sep}bundle{sep}{id}".format(pr=self, sep=sep, id=_id)
             fn_bundle = self._fn_bundle(bid)
             os.makedirs(os.path.dirname(fn_bundle), exist_ok=True)
             with open(fn_bundle, 'w') as file:
@@ -1572,8 +1573,10 @@ class FlowProject(signac.contrib.Project, metaclass=_FlowProjectClass):
 
     def _expand_bundled_jobs(self, scheduler_jobs):
         "Expand jobs which were submitted as part of a bundle."
+        sep = getattr(self._environment, 'JOB_ID_SEPARATOR', '/')
+        bundle_prefix = "{pr}{sep}bundle{sep}".format(pr=self, sep=sep)
         for job in scheduler_jobs:
-            if job.name().startswith('{}/bundle/'.format(self)):
+            if job.name().startswith(bundle_prefix):
                 with open(self._fn_bundle(job.name())) as file:
                     for line in file:
                         yield ClusterJob(line.strip(), job.status())

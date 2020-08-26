3af7e0e7454f1a4a4d46faf5f62a7bfb6b131414
Hardik Ojha <44747868+kidrahahjo@users.noreply.github.com>
2020-08-26 23:27:04 +0530

    Add aggregates.py in flow

---
+++ flow/aggregates.py
@@ -0,0 +1,255 @@
+# Copyright (c) 2020 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+from collections.abc import Iterable
+from hashlib import md5
+from itertools import groupby
+from itertools import zip_longest
+
+from flow import FlowProject
+from signac.contrib.project import Project
+
+
+class Aggregate:
+    """Decorator for operation functions that are to be aggregated.
+    By default, if the aggregator parameter is not passed,
+    an aggregate of all jobs will be created.
+
+    .. code-block:: python
+
+        example_aggregate = Aggregate()
+        @example_aggregate
+        @FlowProject.operation
+        def foo(*jobs):
+            print(len(jobs))
+
+    :param aggregator:
+        Information describing how to aggregate jobs. Is a callable that
+        takes in a list of jobs and can return or yield subsets of jobs as
+        an iterable. The default behavior is creating a single aggregate
+        of all jobs
+    :type aggregator:
+        callable
+    :param sort_by:
+        Before aggregating, sort the jobs by a given statepoint parameter.
+        The default behavior is no sorting.
+    :type sort_by:
+        str or NoneType
+    :param reverse_order:
+        States if the jobs are to be sorted in reverse order.
+        The default value is False.
+    :type reverse_order:
+        bool
+    :param select:
+        Condition for filtering individual jobs. This is passed as the
+        callable argument to `filter`.
+        The default behavior is no filtering.
+    :type select:
+        callable or NoneType
+    """
+
+    def __init__(self, aggregator=None, sort_by=None, reverse_order=False, select=None):
+        if aggregator is None:
+            def aggregator(jobs):
+                return [jobs]
+
+        if not callable(aggregator):
+            raise TypeError(f"Expected callable for aggregator, got {type(aggregator)}")
+
+        if sort_by is not None and not isinstance(sort_by, str):
+            raise TypeError(f"Expected string sort_by parameter, got {type(sort_by)}")
+
+        if select is not None and not callable(select):
+            raise TypeError(f"Expected callable for select, got {type(select)}")
+
+        self._is_aggregate = getattr(aggregator, '_num', 0) != 1
+        self._aggregator = aggregator
+        self._sort_by = sort_by
+        self._reverse_order = reverse_order
+        self._select = select
+
+    @classmethod
+    def groupsof(cls, num=1, sort_by=None, reverse_order=False, select=None):
+        # copied from: https://docs.python.org/3/library/itertools.html#itertools.zip_longest
+        try:
+            num = int(num)
+            if num <= 0:
+                raise ValueError('The num parameter should have a value greater than 0')
+        except TypeError:
+            raise TypeError('The num parameter should be an integer')
+
+        def aggregator(jobs):
+            args = [iter(jobs)] * num
+            return zip_longest(*args)
+
+        if num == 1 and sort_by == reverse_order == select is None:
+            setattr(aggregator, '_num', num)
+
+        return cls(aggregator, sort_by, reverse_order, select)
+
+    @classmethod
+    def groupby(cls, key, default=None, sort_by=None, reverse_order=False, select=None):
+        if isinstance(key, str):
+            if default is None:
+                def keyfunction(job):
+                    return job.sp[key]
+            else:
+                def keyfunction(job):
+                    return job.sp.get(key, default)
+
+        elif isinstance(key, Iterable):
+            keys = list(key)
+
+            if default is None:
+                def keyfunction(job):
+                    return [job.sp[key] for key in keys]
+            else:
+                if isinstance(default, Iterable):
+                    if len(default) != len(keys):
+                        raise ValueError("Expected length of default argument is "
+                                         f"{len(keys)}, got {len(default)}.")
+                else:
+                    raise TypeError("Invalid default argument. Expected Iterable, "
+                                    f"got {type(default)}")
+
+                def keyfunction(job):
+                    return [job.sp.get(key, default_value)
+                            for key, default_value in zip(keys, default)]
+
+        elif callable(key):
+            keyfunction = key
+
+        else:
+            raise TypeError("Invalid key argument. Expected either str, Iterable "
+                            f"or a callable, got {type(key)}")
+
+        def aggregator(jobs):
+            for key, group in groupby(sorted(jobs, key=keyfunction), key=keyfunction):
+                yield group
+
+        return cls(aggregator, sort_by, reverse_order, select)
+
+    def __eq__(self, other):
+        return self._aggregator.__code__.co_code == other._aggregator.__code__.co_code and \
+               self._select.__code__.co_code == other._select.__code__.co_code and \
+               self._sort_by == other._sort_by and self._reverse_order == other._reverse_order
+
+    def _create_StoreAggregates(self):
+        # We create the instance of _StoreAggregates or _StoreDefaultAggregates classes
+        # to make sure that we don't explicitly store the aggregates in the FlowProject.
+        # When iterated over these classes, an aggregate is yielded.
+        if not self._is_aggregate:
+            return _StoreDefaultAggregate()
+        else:
+            return _StoreAggregates(self)
+
+    def __call__(self, func=None):
+        if callable(func):
+            setattr(func, '_flow_aggregate', self._create_StoreAggregates())
+            return func
+        else:
+            raise TypeError('Invalid argument passed while calling '
+                            'the aggregate instance. Expected a callable, '
+                            f'got {type(func)}.')
+
+
+class _StoreAggregates:
+    """This class holds the information of all the aggregates associated with
+    a :py:class:`Aggregate`.
+
+    This is a callable class which, when called, generates all the aggregates.
+    When iterated through it's instance, all the aggregates are yielded.
+
+    :param aggregate:
+        Aggregate object associated with this class.
+    :type aggregate:
+        :py:class:`Aggregate`
+    """
+    def __init__(self, aggregate):
+        self._aggregate = aggregate
+        self._aggregates = list()
+
+    def __iter__(self):
+        yield from self._aggregates
+
+    def __call__(self, jobs):
+        # If the instance of this class is called then we will
+        # generate aggregates and store them in self._aggregates
+        if isinstance(jobs, (FlowProject, Project)):
+            project = jobs
+        else:  # Got a tuple of jobs
+            project = jobs[0]._project
+
+        aggregated_jobs = list(jobs)
+        if self._aggregate._select is not None:
+            aggregated_jobs = list(filter(self._aggregate._select, aggregated_jobs))
+        if self._aggregate._sort_by is not None:
+            aggregated_jobs = list(sorted(aggregated_jobs,
+                                          key=lambda job: job.sp[self._aggregate._sort_by],
+                                          reverse=bool(self._aggregate._reverse_order)))
+
+        aggregated_jobs = self._aggregate._aggregator([job for job in aggregated_jobs])
+        self._create_nested_aggregate_list(aggregated_jobs, project)
+
+    def _create_nested_aggregate_list(self, aggregated_jobs, project):
+        # This method converts the returned subset of jobs as an Iterable
+        # from an aggregator function to a subset of jobs as tuple.
+        aggregated_jobs = list(aggregated_jobs)
+        nested_aggregates = []
+
+        for aggregate in aggregated_jobs:
+            try:
+                filter_aggregate = []
+                for job in aggregate:
+                    if job is None:
+                        continue
+                    elif job not in project:
+                        raise LookupError(f'The signac job {str(job)} not found in {project}')
+                    filter_aggregate.append(job)
+                filter_aggregate = tuple(filter_aggregate)
+                nested_aggregates.append(filter_aggregate)
+            except TypeError:  # aggregate is not iterable
+                raise ValueError("Invalid aggregator function provided by "
+                                 "the user.")
+
+        self._aggregates = nested_aggregates
+
+
+class _StoreDefaultAggregate:
+    """This class holds the information of the project associated with
+    an operation function aggregated by the default aggregates i.e.
+    :py:class:`Aggregate.groupsof(1)`.
+
+    When iterated through it's instance, it yields a tuple of a single job from
+    the Project.
+    """
+    def __init__(self):
+        self._project = None
+
+    def __iter__(self):
+        for job in self._project:
+            yield (job,)
+
+    def __call__(self, jobs):
+        # We have to store self._project is a instance of this class
+        # is called. This is because we will then iterate over that
+        # project in order to return an aggregates of one.
+        if isinstance(jobs, (FlowProject, Project)):
+            self._project = jobs
+        else:  # Got a tuple of jobs
+            self._project = jobs[0]._project
+
+
+def get_aggregate_id(jobs):
+    """Generate hashed id for an aggregate of jobs
+
+    :param jobs:
+        The signac job handles
+    :type jobs:
+        tuple
+    """
+    if len(jobs) == 1:
+        return str(jobs[0])  # Return job id as it's already unique
+
+    blob = ','.join((job.get_id() for job in jobs))
+    return f'agg-{md5(blob.encode()).hexdigest()}'
---
+++ tests/test_aggregates.py
@@ -0,0 +1,264 @@
+import pytest
+
+from functools import partial
+from tempfile import TemporaryDirectory
+
+import signac
+from flow.aggregates import Aggregate
+
+
+class AggregateProjectSetup:
+    project_class = signac.Project
+    entrypoint = dict(path='')
+
+    @pytest.fixture
+    def setUp(self, request):
+        self._tmp_dir = TemporaryDirectory(prefix='flow-aggregate_')
+        request.addfinalizer(self._tmp_dir.cleanup)
+        self.project = self.project_class.init_project(
+            name='AggregateTestProject',
+            root=self._tmp_dir.name)
+
+    def mock_project(self):
+        project = self.project_class.get_project(root=self._tmp_dir.name)
+        for i in range(10):
+            even = i % 2 == 0
+            if even:
+                project.open_job(dict(i=i, half=i / 2, even=even)).init()
+            else:
+                project.open_job(dict(i=i, even=even)).init()
+        return project
+
+    @pytest.fixture
+    def project(self):
+        return self.mock_project()
+
+
+# Test the decorator class Aggregate
+class TestAggregate(AggregateProjectSetup):
+
+    def test_default_init(self):
+        aggregate_instance = Aggregate()
+        test_list = (1, 2, 3, 4, 5)
+        assert aggregate_instance._sort_by is None
+        assert aggregate_instance._aggregator(test_list) == [test_list]
+        assert aggregate_instance._select is None
+
+    def test_invalid_aggregator(self, setUp, project):
+        aggregators = ['str', 1, {}]
+        for aggregator in aggregators:
+            with pytest.raises(TypeError):
+                Aggregate(aggregator)
+
+    def test_invalid_sort_by(self):
+        sort_list = [1, {}, lambda x: x]
+        for sort in sort_list:
+            with pytest.raises(TypeError):
+                Aggregate(sort_by=sort)
+
+    def test_invalid_select(self):
+        selectors = ['str', 1, []]
+        for _select in selectors:
+            with pytest.raises(TypeError):
+                Aggregate(select=_select)
+
+    def test_invalid_call(self):
+        call_params = ['str', 1, None]
+        for param in call_params:
+            with pytest.raises(TypeError):
+                Aggregate()(param)
+
+    def test_call_without_function(self):
+        aggregate_instance = Aggregate()
+        with pytest.raises(TypeError):
+            aggregate_instance()
+
+    def test_call_with_function(self):
+        aggregate_instance = Aggregate()
+
+        def test_function(x):
+            return x
+
+        assert not getattr(test_function, '_flow_aggregate', False)
+        test_function = aggregate_instance(test_function)
+        assert getattr(test_function, '_flow_aggregate', False)
+
+    def test_with_decorator_with_pre_initialization(self):
+        aggregate_instance = Aggregate()
+
+        @aggregate_instance
+        def test_function(x):
+            return x
+
+        assert getattr(test_function, '_flow_aggregate', False)
+
+    def test_with_decorator_without_pre_initialization(self):
+        @Aggregate()
+        def test_function(x):
+            return x
+
+        assert getattr(test_function, '_flow_aggregate', False)
+
+    def test_groups_of_invalid_num(self):
+        invalid_values = [{}, 'str', -1, -1.5]
+        for invalid_value in invalid_values:
+            with pytest.raises((TypeError, ValueError)):
+                Aggregate.groupsof(invalid_value)
+
+    def test_group_by_invalid_key(self):
+        with pytest.raises(TypeError):
+            Aggregate.groupby(1)
+
+    def test_groupby_with_valid_type_default_for_Iterable(self, setUp, project):
+        Aggregate.groupby(['half', 'even'], default=[-1, -1])
+
+    def test_groupby_with_invalid_type_default_key_for_Iterable(self, setUp, project):
+        with pytest.raises(TypeError):
+            Aggregate.groupby(['half', 'even'], default=-1)
+
+    def test_groupby_with_invalid_length_default_key_for_Iterable(self, setUp, project):
+        with pytest.raises(ValueError):
+            Aggregate.groupby(['half', 'even'], default=[-1, -1, -1])
+
+
+# Test the _StoreAggregate and _StoreDefaultAggregate classes
+class TestStoreAggregate(AggregateProjectSetup):
+
+    def test_valid_aggregator_non_partial(self, setUp, project):
+        # Return groups of 1
+        def helper_aggregator(jobs):
+            for job in jobs:
+                yield (job, )
+
+        aggregate_instance = Aggregate(helper_aggregator)
+
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        aggregate_job_manual = helper_aggregator(project)
+        aggregate_instance(project)  # Generate Aggregates
+
+        assert [aggregate for aggregate in aggregate_job_manual] == \
+               list(aggregate_instance)
+
+    def test_valid_aggregator_partial(self, setUp, project):
+        aggregate_instance = Aggregate(lambda jobs: [jobs])
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        aggregate_instance(project)  # Generate Aggregates
+
+        assert [tuple([job for job in project])] == \
+               list(aggregate_instance)
+
+    def test_valid_sort_by(self, setUp, project):
+        helper_sort = partial(sorted, key=lambda job: job.sp.i)
+        aggregate_instance = Aggregate(sort_by='i')
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        aggregate_instance(project)  # Generate Aggregates
+        assert [tuple(helper_sort(project))] == list(aggregate_instance)
+
+    def test_valid_reversed_sort(self, setUp, project):
+        helper_sort = partial(sorted, key=lambda job: job.sp.i, reverse=True)
+        aggregate_instance = Aggregate(sort_by='i', reverse_order=True)
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        aggregate_instance(project)  # Generate Aggregates
+        assert [tuple(helper_sort(project))] == list(aggregate_instance)
+
+    def test_groups_of_valid_num(self, setUp, project):
+        valid_values = [1, 2, 3, 6]
+
+        total_jobs = len(project)
+
+        for valid_value in valid_values:
+            aggregate_instance = Aggregate.groupsof(valid_value)
+            aggregate_instance = aggregate_instance._create_StoreAggregates()
+            aggregate_instance(project)  # Generate Aggregates
+            if total_jobs % valid_value == 0:
+                length_of_aggregate = total_jobs/valid_value
+            else:
+                length_of_aggregate = int(total_jobs/valid_value) + 1
+            assert len(list(aggregate_instance)) == length_of_aggregate
+
+    def test_groupby_with_valid_string_key(self, setUp, project):
+        aggregate_instance = Aggregate.groupby('even')
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        aggregates = 0
+        aggregate_instance(project)  # Generate Aggregates
+        for agg in aggregate_instance:
+            aggregates += 1
+        assert aggregates == 2
+
+    def test_groupby_with_invalid_string_key(self, setUp, project):
+        aggregate_instance = Aggregate.groupby('invalid_key')
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        with pytest.raises(KeyError):
+            # We will attempt to generate aggregates but will fail in
+            # doing so due to the invalid key
+            aggregate_instance(project)
+
+    def test_groupby_with_default_key_for_string(self, setUp, project):
+        aggregate_instance = Aggregate.groupby('half', default=-1)
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        aggregate_instance(project)  # Generate Aggregates
+        aggregates = 0
+        for agg in aggregate_instance:
+            aggregates += 1
+        assert aggregates == 6
+
+    def test_groupby_with_Iterable_key(self, setUp, project):
+        aggregate_instance = Aggregate.groupby(['i', 'even'])
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        aggregate_instance(project)  # Generate Aggregates
+        aggregates = 0
+        for agg in aggregate_instance:
+            aggregates += 1
+        assert aggregates == 10
+
+    def test_groupby_with_invalid_Iterable_key(self, setUp, project):
+        aggregate_instance = Aggregate.groupby(['half', 'even'])
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        with pytest.raises(KeyError):
+            # We will attempt to generate aggregates but will fail in
+            # doing so due to the invalid keys
+            aggregate_instance(project)
+
+    def test_groupby_with_valid_default_key_for_Iterable(self, setUp, project):
+        aggregate_instance = Aggregate.groupby(['half', 'even'], default=[-1, -1])
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        aggregate_instance(project)  # Generate Aggregates
+        aggregates = 0
+        for agg in aggregate_instance:
+            aggregates += 1
+        assert aggregates == 6
+
+    def test_groupby_with_callable_key(self, setUp, project):
+        def keyfunction(job):
+            return job.sp['even']
+
+        aggregate_instance = Aggregate.groupby(keyfunction)
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        aggregate_instance(project)  # Generate Aggregates
+        aggregates = 0
+        for agg in aggregate_instance:
+            aggregates += 1
+        assert aggregates == 2
+
+    def test_groupby_with_invalid_callable_key(self, setUp, project):
+        def keyfunction(job):
+            return job.sp['half']
+        aggregate_instance = Aggregate.groupby(keyfunction)
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        with pytest.raises(KeyError):
+            # We will attempt to generate aggregates but will fail in
+            # doing so due to the invalid key
+            aggregate_instance(project)
+
+    def test_valid_select(self, setUp, project):
+        def _select(job):
+            return job.sp.i > 5
+
+        aggregate_instance = Aggregate.groupsof(1, select=_select)
+        aggregate_instance = aggregate_instance._create_StoreAggregates()
+        aggregate_instance(project)
+        selected_jobs = []
+        for job in project:
+            if _select(job):
+                selected_jobs.append((job,))
+        assert list(aggregate_instance) == selected_jobs

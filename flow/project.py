e2b53ad1410e7e4a9314865486e7ccbad49c2314
Bradley Dice <bdice@bradleydice.com>
2020-12-28 11:59:22 -0600

    Update comment.

--- flow/project.py
+++ flow/project.py
@@ -294,7 +294,9 @@ class _AggregatesCursor:
             return False
         if self._filter is None and self._doc_filter is None:
             # Using the Project's __contains__ method is fastest if no
-            # filtering is needed
+            # filtering is needed. This is a backport of a PR to signac that
+            # optimizes the JobsCursor __contains__ method, and is required for
+            # sufficient performance with earlier versions of signac.
             return aggregate[0] in self._project
         # Slow path: requires O(N) iteration over the JobsCursor
         return aggregate[0] in self._jobs_cursor

e30306fdaf1e71bc7a5fc63b54456119f2248f55
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2021-04-04 11:23:12 -0700

    Add default schema_version to config.

--- flow/util/config.py
+++ flow/util/config.py
@@ -14,6 +14,7 @@ status_performance_warn_threshold = float(default=0.2)
 show_traceback = boolean()
 eligible_jobs_max_lines = int(default=10)
 status_parallelization = string(default='none')
+schema_version = int(default=1)
 """
 
 

d04588a52f41f23f5014ca2f3f1cdcf5b37a467a
Bradley Dice <bdice@bradleydice.com>
2021-04-23 14:16:48 -0500

    Fix failing tests by matching previous output.

--- flow/templates/summit.sh
+++ flow/templates/summit.sh
@@ -1,14 +1,14 @@
 {# Templated in accordance with: https://www.olcf.ornl.gov/for-users/system-user-guides/summit/running-jobs/ #}
 {% extends "lsf.sh" %}
 {% block tasks %}
-    {% set threshold = 0 if force else 0.9 %}
-    {% set nn = operations|map('guess_resource_sets')|calc_num_nodes(parallel) %}
+    {%- set threshold = 0 if force else 0.9 %}
+    {%- set nn = operations|map('guess_resource_sets')|calc_num_nodes(parallel) %}
 #BSUB -nnodes {{ nn }}
 {% endblock %}
 {% block header %}
-    {{ super() -}}
-    {% set account = account|default(environment|get_account_name, true) %}
-    {% if account %}
+    {{- super() -}}
+    {%- set account = account|default(environment|get_account_name, true) %}
+    {%- if account %}
 #BSUB -P {{ account }}
-    {% endif %}
+    {%- endif %}
 {% endblock %}

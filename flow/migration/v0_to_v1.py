b5f935ee7573aff99ed106692fbc4b19bae1d9fc
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2021-04-25 11:00:09 -0700

    Update copyrights.

--- flow/migration/v0_to_v1.py
+++ flow/migration/v0_to_v1.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 The Regents of the University of Michigan
+# Copyright (c) 2021 The Regents of the University of Michigan
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 """Migrate from schema version 0 to version 1.

b9b61fb1afddc8556bf252f92aba169368dbe352
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2021-04-25 08:17:08 -0700

    Fix logic to get the flow schema version rather than signac's.

--- flow/migration/__init__.py
+++ flow/migration/__init__.py
@@ -14,6 +14,8 @@ from signac.common.config import get_config
 from ...version import SCHEMA_VERSION, __version__
 from .v0_to_v1 import migrate_v0_to_v1
 
+from flow.util.config import get_config_value
+
 FN_MIGRATION_LOCKFILE = ".FLOW_PROJECT_MIGRATION_LOCK"
 
 
@@ -58,28 +60,31 @@ def _lock_for_migration(project):
 def _collect_migrations(project):
     schema_version = version.parse(SCHEMA_VERSION)
 
-    def config_schema_version():
-        return version.parse(project._config["schema_version"])
+    def get_config_schema_version():
+        # TODO: The means of getting schema versions will have to change for
+        # flow versions in schema versions > 1 that no longer rely on signac's
+        # configuration file and schema.
+        return version.parse(get_config_value("schema_version"))
 
-    if config_schema_version() > schema_version:
+    if get_config_schema_version() > schema_version:
         # Project config schema version is newer and therefore not supported.
         raise RuntimeError(
             "The signac-flow schema version used by this project is {}, but flow {} "
             "only supports up to schema version {}. Try updating flow.".format(
-                config_schema_version, __version__, SCHEMA_VERSION
+                get_config_schema_version, __version__, SCHEMA_VERSION
             )
         )
 
-    while config_schema_version() < schema_version:
+    while get_config_schema_version() < schema_version:
         for (origin, destination), migration in MIGRATIONS.items():
-            if version.parse(origin) == config_schema_version():
+            if version.parse(origin) == get_config_schema_version():
                 yield (origin, destination), migration
                 break
         else:
             raise RuntimeError(
                 "The signac-flow schema version used by this project is {}, but flow {} "
                 "uses schema version {} and does not know how to migrate.".format(
-                    config_schema_version(), __version__, schema_version
+                    get_config_schema_version(), __version__, schema_version
                 )
             )
 

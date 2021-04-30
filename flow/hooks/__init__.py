2daa4c7f697693a1a68e78b0b78d3bb469577b00
Bradley Dice <bdice@bradleydice.com>
2021-04-30 10:19:15 -0500

    Use explicit keyword names.

--- flow/hooks/__init__.py
+++ flow/hooks/__init__.py
@@ -38,9 +38,16 @@ class Hooks:
         "on_fail",
     ]
 
-    def __init__(self, **kwargs):
-        for hook_trigger in self._hook_triggers:
-            setattr(self, hook_trigger, _HooksList(kwargs.get(hook_trigger, [])))
+    def __init__(self, *, on_start=None, on_finish=None, on_success=None, on_fail=None):
+        def set_hooks(self, trigger_name, trigger_value):
+            if trigger_value is None:
+                trigger_value = []
+            setattr(self, trigger_name, _HooksList(trigger_value))
+
+        set_hooks(self, "on_start", on_start)
+        set_hooks(self, "on_finish", on_finish)
+        set_hooks(self, "on_success", on_success)
+        set_hooks(self, "on_fail", on_fail)
 
     def __setattr__(self, name, value):
         """Convert to _HooksList when setting a hook trigger attribute."""

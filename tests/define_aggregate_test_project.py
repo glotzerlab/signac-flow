6266b3728188381fc4af60dadd9fc9f074f9dab1
Bradley Dice <bdice@bradleydice.com>
2021-05-14 10:22:22 -0500

    Simplify tests with set_all_job_docs.

--- tests/define_aggregate_test_project.py
+++ tests/define_aggregate_test_project.py
@@ -11,6 +11,11 @@ def statepoint_i_even_odd_aggregator(jobs):
     return [even, odd]
 
 
+def set_all_job_docs(jobs, key, value):
+    for job in jobs:
+        job.document[key] = value
+
+
 group1 = _AggregateTestProject.make_group(
     name="group_agg", group_aggregator=aggregator()
 )
@@ -25,40 +30,35 @@ def op1(job):
 @aggregator.groupby("even")
 def agg_op1(*jobs):
     total = sum(job.sp.i for job in jobs)
-    for job in jobs:
-        job.document.sum = total
+    set_all_job_docs(jobs, "sum", total)
 
 
 @_AggregateTestProject.operation
 @aggregator.groupby(lambda job: job.sp.i % 2)
 def agg_op1_different(*jobs):
     sum_other = sum(job.sp.i for job in jobs)
-    for job in jobs:
-        job.document.sum_other = sum_other
+    set_all_job_docs(jobs, "sum_other", sum_other)
 
 
 @_AggregateTestProject.operation
 @aggregator(statepoint_i_even_odd_aggregator)
 def agg_op1_custom(*jobs):
     sum_custom = sum(job.sp.i for job in jobs)
-    for job in jobs:
-        job.document.sum_custom = sum_custom
+    set_all_job_docs(jobs, "sum_custom", sum_custom)
 
 
 @_AggregateTestProject.operation
 @group1
 @aggregator.groupsof(30)
 def agg_op2(*jobs):
-    for job in jobs:
-        job.document.op2 = True
+    set_all_job_docs(jobs, "op2", True)
 
 
 @_AggregateTestProject.operation
 @group1
 @aggregator()
 def agg_op3(*jobs):
-    for job in jobs:
-        job.document.op3 = True
+    set_all_job_docs(jobs, "op3", True)
 
 
 @_AggregateTestProject.operation

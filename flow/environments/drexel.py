8e785fc3e5d4c031a141beebaf7e9b7e651f311b
Bradley Dice <bdice@bradleydice.com>
2021-08-16 09:57:52 -0500

    Rewrite note.

--- flow/environments/drexel.py
+++ flow/environments/drexel.py
@@ -8,7 +8,8 @@ from ..environment import DefaultSlurmEnvironment
 class PicotteEnvironment(DefaultSlurmEnvironment):
     """Environment profile for the Picotte supercomputer.
 
-    https://proteusmaster.urcf.drexel.edu/urcfwiki/ (this link may require access to the Drexel University network)
+    https://proteusmaster.urcf.drexel.edu/urcfwiki/
+    Note: This link may require access to the Drexel University network.
     """
 
     hostname_pattern = r".*\.cm\.cluster$"

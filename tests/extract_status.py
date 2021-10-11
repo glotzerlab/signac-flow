599fd1626d6f83e145cfa92288fea7d33658936b
Charlotte Shiqi Zhao <56694726+Charlottez112@users.noreply.github.com>
2021-10-11 09:50:33 -0400

    Add tests for print_status output (#562)
    * Add tests
    
    * fixed generate reference status
    
    * fixed test_status.py
    
    * Address comments
    
    * Add more print_status options
    
    * add status_reference_data.tar.gz
    
    * Add 'profile' option
    
    * Add pprofile to testing dependencies.
    
    * Add pprofile to oldest test requirements.
    
    * Revert pprofile additions.
    
    * Delete tests for profiling
    
    * Remove unused code.
    
    * Remove extraneous return.
    
    * Use f-strings.
    
    * Simplify _TestProject
    
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

---
+++ tests/extract_status.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# Copyright (c) 2021 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+"""Extract generated status into a signac project for simplified inspection."""
+
+import argparse
+import os
+
+import generate_status_reference_data as gen
+import signac
+
+PROJECT_DIR = os.path.join(os.path.dirname(__file__), "./status_reference_data")
+
+
+def main(args):
+    if not os.path.exists(PROJECT_DIR):
+        os.makedirs(PROJECT_DIR)
+    elif args.force:
+        import shutil
+
+        shutil.rmtree(PROJECT_DIR)
+        os.makedirs(PROJECT_DIR)
+    else:
+        return
+
+    p = signac.init_project(name=gen.STATUS_OPTIONS_PROJECT_NAME, root=PROJECT_DIR)
+    p.import_from(origin=gen.ARCHIVE_PATH)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract reference status output")
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="Recreate the unarchived data space even if the directory already exists.",
+    )
+    main(parser.parse_args())

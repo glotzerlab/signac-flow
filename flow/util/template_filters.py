625a16b8fa758adca77b9987b63432cc29fc7622
Brandon Butler <butlerbr@umich.edu>
2022-07-06 14:19:43 -0400

    fix: add project to get_config_value call

--- flow/util/template_filters.py
+++ flow/util/template_filters.py
@@ -330,7 +330,7 @@ def get_account_name(project, required=False):
     """
     env_name = project._environment.__name__
     try:
-        return get_config_value("account", ns=env_name)
+        return get_config_value(project, "account", ns=env_name)
     except ConfigKeyError as error:
         ACCOUNT_MESSAGE = (
             "Environment '{env}' {requires_or_allows} the specification of an "

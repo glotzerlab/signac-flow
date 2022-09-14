43e4abb55a27cad185ba1c946da027952c5d99af
iblanco11981870 <83680829+iblanco11981870@users.noreply.github.com>
2022-09-14 14:44:17 -0400

    Use pytest fixtures to initialize the bd array

--- tests/test_util.py
+++ tests/test_util.py
@@ -13,79 +13,74 @@ def mock_operations():
     return [MockOp(1), MockOp(8), MockOp()]
 
 
+@pytest.fixture()
+def bd():
+    return _bidict({"a": 1, "b": 2})
+
+
 class TestBidict:
+    def test_basic_dict(self, bd):
 
-    bd = _bidict({"a": 1, "b": 2})
+        bd["c"] = 1
+        assert bd == {"a": 1, "c": 1, "b": 2}
+        assert bd.inverse == {1: ["a", "c"], 2: ["b"]}
+        assert len(bd) == 3
+        assert list(bd.keys()) == ["a", "b", "c"]
+        assert list(bd.values()) == [1, 2, 1]
+        assert list(bd.items()) == [("a", 1), ("b", 2), ("c", 1)]
 
-    def add_c_bidict(self):
-        self.bd["c"] = 1
+    def test_get_func(self, bd):
+        assert bd.get("a") == 1
+        assert bd.get("q", 42) == 42
 
-    def test_basic_dict(self):
-        self.add_c_bidict()
-        assert self.bd == {"a": 1, "c": 1, "b": 2}
-        assert self.bd.inverse == {1: ["a", "c"], 2: ["b"]}
-        assert len(self.bd) == 3
-        assert list(self.bd.keys()) == ["a", "b", "c"]
-        assert list(self.bd.values()) == [1, 2, 1]
-        assert list(self.bd.items()) == [("a", 1), ("b", 2), ("c", 1)]
+    def test_belonging_func(self, bd):
+        assert "a" in bd
+        assert "q" not in bd
 
-    def test_get_func(self):
-        assert self.bd.get("a") == 1
-        assert self.bd.get("q", 42) == 42
+    def test_del_c(self, bd):
+        bd["c"] = 1
+        del bd["c"]
+        assert bd == {"a": 1, "b": 2}
+        assert bd.inverse == {1: ["a"], 2: ["b"]}
+        assert len(bd) == 2
 
-    def test_belonging_func(self):
-        assert "a" in self.bd
-        assert "q" not in self.bd
+    def test_add_b(self, bd):
+        bd["b"] = 3
+        assert bd == {"a": 1, "b": 3}
+        assert bd.inverse == {3: ["b"], 1: ["a"]}
 
-    def test_del_c(self):
-        self.add_c_bidict()
-        del self.bd["c"]
-        assert self.bd == {"a": 1, "b": 2}
-        assert self.bd.inverse == {1: ["a"], 2: ["b"]}
-        assert len(self.bd) == 2
+    def test_update(self, bd):
+        bd.update({"c": 2})
+        assert bd == {"b": 2, "c": 2, "a": 1}
+        assert bd.inverse == {1: ["a"], 2: ["b", "c"]}
 
-    def test_del_a(self):
-        del self.bd["a"]
-        assert self.bd == {"b": 2}
-        assert self.bd.inverse == {2: ["b"]}
-
-    def test_add_b(self):
-        self.bd["b"] = 3
-        assert self.bd == {"b": 3}
-        assert self.bd.inverse == {3: ["b"]}
-
-    def test_update(self):
-        self.bd.update({"c": 2})
-        assert self.bd == {"b": 3, "c": 2}
-        assert self.bd.inverse == {2: ["c"], 3: ["b"]}
-
-    def test_popitem(self):
+    def test_popitem(self, bd):
         # Note that MutableMapping's popitem is *not* LIFO-ordered, it pops
         # according to the key iteration order (which means it defaults to FIFO
         # for the underlying dict, which is insertion-ordered).
-        assert self.bd.popitem() == ("b", 3)
-        assert self.bd == {"c": 2}
-        assert self.bd.inverse == {2: ["c"]}
+        assert bd.popitem() == ("a", 1)
+        assert bd == {"b": 2}
+        assert bd.inverse == {2: ["b"]}
 
-    def test_set_default_overlap(self):
-        self.bd.setdefault("c", 7)
-        assert self.bd == {"c": 2}
-        assert self.bd.inverse == {2: ["c"]}
+    def test_set_default_overlap(self, bd):
+        bd.setdefault("b", 7)
+        assert bd == {"b": 2, "a": 1}
+        assert bd.inverse == {1: ["a"], 2: ["b"]}
 
-    def test_default_no_overlap(self):
-        self.bd.setdefault("a", 9)
-        assert self.bd == {"c": 2, "a": 9}
-        assert self.bd.inverse == {2: ["c"], 9: ["a"]}
+    def test_default_no_overlap(self, bd):
+        bd.setdefault("c", 9)
+        assert bd == {"a": 1, "b": 2, "c": 9}
+        assert bd.inverse == {2: ["b"], 9: ["c"], 1: ["a"]}
 
-    def test_pop(self):
-        assert self.bd.pop("c") == 2
-        assert self.bd == {"a": 9}
-        assert self.bd.inverse == {9: ["a"]}
+    def test_pop(self, bd):
+        assert bd.pop("b") == 2
+        assert bd == {"a": 1}
+        assert bd.inverse == {1: ["a"]}
 
-    def test_clear(self):
-        self.bd.clear()
-        assert self.bd == {}
-        assert self.bd.inverse == {}
+    def test_clear(self, bd):
+        bd.clear()
+        assert bd == {}
+        assert bd.inverse == {}
 
 
 class TestTemplateFilters:

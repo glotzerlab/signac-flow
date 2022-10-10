1e97e2ba20c1ba7ea02e7a17927f4dbda10527a4
Brandon Butler <butlerbr@umich.edu>
2022-10-10 11:41:34 -0400

    refactor: Remove __call__ method of LogOperation
    The call method is confusing as it only applies to projects but hooks
    can be project or operation wide.

--- flow/hooks/log_operations.py
+++ flow/hooks/log_operations.py
@@ -84,5 +84,3 @@ class LogOperations:
         project.project_hooks.on_success.append(self.on_success)
         project.project_hooks.on_exception.append(self.on_exception)
         return project
-
-    __call__ = install_project_hooks

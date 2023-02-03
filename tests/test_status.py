8e906baac524c7e0be73e4eb2f7c0a5bb201ab28
Brandon Butler <butlerbr@umich.edu>
2023-02-03 12:18:17 -0500

    ci: Update pre-commit config

--- tests/test_status.py
+++ tests/test_status.py
@@ -10,7 +10,6 @@ import signac
 
 
 def test_print_status():
-
     # Must import the data into the project.
     with signac.TemporaryProject(name=gen.PROJECT_NAME) as p, signac.TemporaryProject(
         name=gen.STATUS_OPTIONS_PROJECT_NAME

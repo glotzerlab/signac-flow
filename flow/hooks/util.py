d330db1cf6f8cbcb6f56d857c23bc1d9ec903ac9
Melody Zhang <yiyuanmz@umich.edu>
2023-02-15 11:36:44 -0500

    added util.py file

---
+++ flow/hooks/util.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2018 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+"""TO DO."""
+from datetime import datetime, timezone
+
+
+def collect_metadata(operation):
+    """TO DO."""
+    return {
+        # the metadata schema version:
+        "_schema_version": "1",
+        "time": datetime.now(timezone.utc).isoformat(),
+        "project": {
+            "path": operation.job._project.root_directory(),
+            # the project schema version:
+            "schema_version": operation.job._project.config.get("schema_version"),
+        },
+        "job-operation": {
+            "name": operation.name,
+            "cmd": operation.cmd,
+            "directives": operation.directives,
+            "job_id": operation.job.get_id(),
+        },
+    }

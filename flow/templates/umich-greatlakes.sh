550696edeb4588a1592c6fde7b174c7b4aae1b72
Brandon Butler <butlerbr@umich.edu>
2023-02-27 10:47:03 -0500

    refactor: Port some templates over as examples of new style.

--- flow/templates/umich-greatlakes.sh
+++ flow/templates/umich-greatlakes.sh
@@ -1,22 +1,16 @@
 {% extends "slurm.sh" %}
 {% set partition = partition|default('standard', true) %}
 {% block tasks %}
-    {% set threshold = 0 if force else 0.9 %}
-    {% set cpu_tasks = operations|calc_tasks('np', parallel, force) %}
-    {% set gpu_tasks = operations|calc_tasks('ngpu', parallel, force) %}
-    {% if gpu_tasks and 'gpu' not in partition and not force %}
+    {% if resources.gpu_tasks and 'gpu' not in partition and not force %}
         {% raise "Requesting GPUs requires a gpu partition!" %}
     {% endif %}
-    {% set nn_cpu = cpu_tasks|calc_num_nodes(36) if 'gpu' not in partition else cpu_tasks|calc_num_nodes(40) %}
-    {% set nn_gpu = gpu_tasks|calc_num_nodes(2) if 'gpu' in partition else 0 %}
-    {% set nn = nn|default((nn_cpu, nn_gpu)|max, true) %}
     {% if partition == 'gpu' %}
-#SBATCH --nodes={{ nn|default(1, true) }}
-#SBATCH --ntasks-per-node={{ (gpu_tasks, cpu_tasks)|max }}
-#SBATCH --gpus={{ gpu_tasks }}
+#SBATCH --nodes={{ resources.num_nodes }}
+#SBATCH --ntasks={{ (resources.ngpu_tasks, resources.ncpu_tasks)|max }}
+#SBATCH --gpus={{ resources.ngpu_tasks }}
     {% else %}{# standard compute partition #}
-#SBATCH --nodes={{ nn }}
-#SBATCH --ntasks-per-node={{ (36, cpu_tasks)|min }}
+#SBATCH --nodes={{ resources.num_nodes }}
+#SBATCH --ntasks-per-node={{ resources.ncpu_tasks }}
     {% endif %}
 {% endblock tasks %}
 {% block header %}

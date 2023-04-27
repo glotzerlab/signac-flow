a5bba490dc73838d8c4d1103925696efce99c0b5
Brandon Butler <butlerbr@umich.edu>
2023-04-27 10:44:17 -0400

    Add Frontier Environment (#743)
    * feat: Add the Frontier supercomputer to environments.
    
    * test: Add Frontier to template testing.
    
    * test: Update environment test template generation to signac 2.0
    
    * doc: Update changelog
    
    * doc: Add Frontier documentation.
    
    * doc: Update incode comment clarity
    
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>
    
    ---------
    
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

---
+++ doc/supported_environments/frontier.rst
@@ -0,0 +1,12 @@
+.. _frontier:
+
+OLCF Frontier
+=============
+
+`Link to official documentation <https://docs.olcf.ornl.gov/systems/frontier_user_guide.html>`_
+
+.. autoclass:: flow.environments.incite.FrontierEnvironment
+
+.. literalinclude:: ../../flow/templates/frontier.sh
+   :caption: frontier.sh
+   :language: jinja
---
+++ flow/templates/frontier.sh
@@ -0,0 +1,19 @@
+{# Templated in accordance with: https://docs.olcf.ornl.gov/systems/crusher_quick_start_guide.html #}
+{% extends "slurm.sh" %}
+{% block tasks %}
+    {% set threshold = 0 if force else 0.9 %}
+    {% set cpu_tasks = operations|calc_tasks('np', parallel, force) %}
+    {% set gpu_tasks = operations|calc_tasks('ngpu', parallel, force) %}
+    {% set nn = gpu_tasks|calc_num_nodes(cpu_tasks, threshold) %}
+    {% if gpu_tasks < 1 and not force %}
+        {% raise "Must request GPUs to use Frontier." %}
+    {% endif %}
+#SBATCH --nodes={{ nn }}
+{% endblock tasks %}
+{% block header %}
+    {{- super() -}}
+    {% set account = account|default(project|get_account_name, true) %}
+    {% if account %}
+#SBATCH --account={{ account }}
+    {% endif %}
+{% endblock header %}

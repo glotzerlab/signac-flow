3687c0c73a0edcead1a00a02344adc39be6416dc
Brandon Butler <butlerbr@umich.edu>
2023-07-27 12:41:27 -0400

    fix: Correctly GPU raise errors in Greatlakes template
    Error when GPU partition is requested without GPUs and when GPUs are
    requested without a GPU partition.

--- flow/templates/umich-greatlakes.sh
+++ flow/templates/umich-greatlakes.sh
@@ -1,8 +1,11 @@
 {% extends "slurm.sh" %}
 {% set partition = partition|default('standard', true) %}
 {% block tasks %}
-    {% if resources.gpu_tasks and 'gpu' not in partition and not force %}
+    {% if resources.ngpu_tasks and 'gpu' not in partition and not force %}
         {% raise "Requesting GPUs requires a gpu partition!" %}
+    {% endif %}
+    {% if 'gpu' in partition and resources.ngpu_tasks == 0 and not force %}
+        {% raise "Requesting gpu partition without GPUs!" %}
     {% endif %}
 #SBATCH --nodes={{ resources.num_nodes }}
     {% if partition == 'gpu' %}

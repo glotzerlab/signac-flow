3bece4ec2278b5fb83a40b8aa6b06d66d0abd66d
Joshua A. Anderson <joaander@umich.edu>
2023-10-30 10:49:38 -0400

    Allow sbatch to error on invliad job configurations.
    Also, work around a core binding issue on Anvil.

--- flow/templates/anvil.sh
+++ flow/templates/anvil.sh
@@ -1,23 +1,10 @@
 {# Templated in accordance with: https://www.rcac.purdue.edu/knowledge/anvil/ #}
 {% extends "slurm.sh" %}
 {% block tasks %}
-    {% if resources.ngpu_tasks %}
-        {% if not ('gpu' in partition or force) %}
-            {% raise "GPU operations require a gpu partition!" %}
-        {% endif %}
-        {% if resources.ngpu_tasks > 4 %}
-            {% raise "Cannot request a gpu job with more than 4 GPUs." %}
-        {% endif %}
-    {% else %}
+    {% if resources.ngpu_tasks == 0 %}
         {% if 'gpu' in partition and not force %}
             {% raise "Requesting gpu partition, but no GPUs requested!" %}
         {% endif %}
-    {% endif %}
-    {% if partition in ['debug', 'shared', 'highmem'] and resources.ncpu_tasks > 128 %}
-        {% raise "Cannot request a shared partition with more than 128 CPU tasks." %}
-    {% endif %}
-    {% if resources.num_nodes > 1 or resources.ncpu_tasks >= 128 or resources.ngpu_tasks >= 4 %}
-#SBATCH -N {{ resources.num_nodes }}
     {% endif %}
 #SBATCH --ntasks={{ resources.ncpu_tasks }}
     {% if 'gpu' in partition %}
@@ -28,6 +15,9 @@
     {{- super() -}}
     {% set account = account|default(project|get_account_name, true) %}
     {% if account %}
-#SBATCH -A {{ account }}
+#SBATCH --account {{ account }}
     {% endif %}
+# As of 2023-10-30, Anvil incorrectly binds ranks to cores with `mpirun -n`.
+# Disable core binding to work around this issue.
+export OMPI_MCA_hwloc_base_binding_policy=""
 {% endblock header %}

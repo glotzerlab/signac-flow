c3ccf3d3c80c1ea62e57f4adf4aaeadf0500dfb9
Brandon Butler <butlerbr@umich.edu>
2023-11-06 17:00:26 -0500

    test: Test new errors in _Partition

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -2,8 +2,34 @@
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 
+import conftest
+import pytest
+
+import flow
 from flow import get_environment
-from flow.environment import ComputeEnvironment, TestEnvironment
+from flow.environment import (
+    ComputeEnvironment,
+    TestEnvironment,
+    _NodeTypes,
+    _PartitionConfig,
+)
+from flow.errors import SubmitError
+
+
+class MockEnvironment(ComputeEnvironment):
+    scheduler_type = conftest.MockScheduler
+    _partition_config = _PartitionConfig(
+        cpus_per_node={"cpu-shared": 100, "cpu-wholenode": 100, "default": 40},
+        gpus_per_node={"gpu": 4},
+        node_types={
+            "cpu-shared": _NodeTypes.SHARED,
+            "cpu-wholenode": _NodeTypes.WHOLENODE,
+        },
+    )
+
+    @classmethod
+    def is_present(cls):
+        return True
 
 
 class TestProject:
@@ -13,3 +39,54 @@ class TestProject:
         assert not issubclass(env, TestEnvironment)
         env = get_environment(test=True)
         assert issubclass(env, TestEnvironment)
+
+
+class TestEnvironments(conftest.TestProjectBase):
+    class Project(flow.FlowProject):
+        pass
+
+    @Project.operation(directives={"ngpu": 1})
+    def gpu_op(job):
+        pass
+
+    @Project.operation(directives={"np": 1_000})
+    def large_cpu_op(job):
+        pass
+
+    @Project.operation(directives={"np": 1})
+    def small_cpu_op(job):
+        pass
+
+    project_class = Project
+
+    def mock_project(self):
+        project = self.project_class.get_project(path=self._tmp_dir.name)
+        project.open_job({"i": 0}).init()
+        project._environment = MockEnvironment
+        return project
+
+    def test_gpu_parttion_without_gpu(self):
+        pr = self.mock_project()
+        with pytest.raises(SubmitError):
+            pr.submit(names=["small_cpu_op"], partition="gpu")
+
+    def test_gpu_op_without_gpu_partition(self):
+        pr = self.mock_project()
+        with pytest.raises(SubmitError):
+            pr.submit(names=["gpu_op"], partition="cpu-shared")
+
+    def test_wholenode_submission_with_insufficient_resources(self):
+        pr = self.mock_project()
+        with pytest.raises(RuntimeError):
+            pr.submit(names=["small_cpu_op"], partition="cpu-wholenode")
+
+    def test_shared_submission_with_too_large_request(self):
+        pr = self.mock_project()
+        with pytest.raises(RuntimeError):
+            pr.submit(names=["large_cpu_op"], partition="cpu-shared")
+
+    def test_various_valid_submissions(self):
+        pr = self.mock_project()
+        pr.submit(names=["large_cpu_op"], partition="cpu-wholenode")
+        pr.submit(names=["small_cpu_op"], partition="cpu-shared")
+        pr.submit(names=["gpu_op"], partition="gpu")

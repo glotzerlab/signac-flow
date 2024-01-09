b448ddede5d9348a365564f7e7c7c5af126641d7
Brandon Butler <butlerbr@umich.edu>
2024-01-09 15:08:34 -0500

    doc: Use Sphinx's build-in deprecation warning system.

--- flow/environments/xsede.py
+++ flow/environments/xsede.py
@@ -21,9 +21,8 @@ _STAMPEDE_OFFSET = os.environ.get("_FLOW_STAMPEDE_OFFSET_`", 0)
 class Stampede2Environment(DefaultSlurmEnvironment):
     """Environment profile for the Stampede2 supercomputer.
 
-    Warning
-    -------
-    This environments is deprecated and will be removed in 0.28.0.
+    .. deprecated:: 0.27.0
+        Stampede2Environment will be removed in 0.28.0.
 
     https://www.tacc.utexas.edu/systems/stampede2
     """
